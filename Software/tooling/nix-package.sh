2446755133ae03bf9a280bc4d67216f53242bbbf
James N <59348282+RandomSpaceship@users.noreply.github.com>
2024-10-12 02:04:52 +1000

    nix packaging script now automatically formats and commits

--- Software/tooling/nix-package.sh
+++ Software/tooling/nix-package.sh
@@ -1,13 +1,45 @@
 # cd to the script location so everything searches in and goes to the right place
-SCRIPT_PATH="$(dirname $0)"
-cd $SCRIPT_PATH/..
+SCRIPT_DIR="$(dirname "$(readlink -f $0)")"
+WORKSPACE_ROOT="$(dirname "$SCRIPT_DIR")"
+cd $WORKSPACE_ROOT
 
-OUTPUT_DIR=ros_ws/nix-packages
+ROS_WS=${WORKSPACE_ROOT}/ros_ws
+OUTPUT_DIR=$ROS_WS/nix-packages
 
+echo "Cleaning old packaging files"
 rm -r $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
+
+echo "Running ros2nix to generate new packaging files..."
 # need to keep the environment isolated so that direnv doesn't screw with ros2nix.
 # -i (--ignore-environment) clears all environment variables and drops you into a shell
 # --command automatically runs the following command and exits the shell once it completes
 # nix run is the "better" way to do this, but it doesn't allow for complete environment isolation like the shell -i flag.
-nix shell -i github:wentasah/ros2nix --command ros2nix --output-dir=$OUTPUT_DIR --output-as-nix-pkg-name --no-default --flake --distro humble $(find -name package.xml)
+nix shell -i github:wentasah/ros2nix --command ros2nix --output-dir=$OUTPUT_DIR --output-as-nix-pkg-name --no-default --distro humble $(find -name package.xml)
+echo "Formatting generated files"
+cd $OUTPUT_DIR
+nix fmt --quiet
+# cd $WORKSPACE_ROOT/native
+# nix fmt --quiet
+# cd $WORKSPACE_ROOT/shared
+# nix fmt --quiet
+# cd $WORKSPACE_ROOT/machines
+# nix fmt --quiet
+
+if ! git diff --cached --quiet >/dev/null
+then
+    HAS_GIT_STAGING=1
+    echo "Stashing staged changes"
+    git stash push -S >/dev/null
+fi
+
+echo "Staging changes"
+git add "${OUTPUT_DIR}"
+echo "Committing changes"
+git commit -m "$(date +%Y-%m-%dT%H:%M:%S) Nix packaging generation" >/dev/null
+
+if [ -v HAS_GIT_STAGING ]
+then
+    echo "Restoring git stash"
+    git stash pop >/dev/null
+fi
