c78bb1c9a7ad00dd54c18b4e665f04f60f336ba4
James N <59348282+RandomSpaceship@users.noreply.github.com>
2024-10-13 15:20:49 +1000

    Add beacon/excavation files to hardware_interfaces package

---
+++ Software/ros_ws/src/hardware_interfaces/src/excavation.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <hi_can_lib.hpp>
+
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv;
+
+    printf("hello world hardware_interfaces package - excavation\n");
+    return 0;
+}
