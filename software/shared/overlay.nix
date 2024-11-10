a73e2f85a7cce1e0cd4ed3bd6fa956ecdb0ef2b3
James N <59348282+RandomSpaceship@users.noreply.github.com>
2024-11-10 21:07:17 +1000

    Add file descriptor wrapper class

--- software/shared/overlay.nix
+++ software/shared/overlay.nix
@@ -1,5 +1,6 @@
 final: prev: {
-  hi-can-lib = prev.callPackage ../shared/hi-can-lib { };
-  hi-can-raw = prev.callPackage ../shared/hi-can-raw { };
-  hi-can-net = prev.callPackage ../shared/hi-can-net { };
+  fd-wrapper = final.callPackage ./fd-wrapper { };
+  hi-can-lib = final.callPackage ./hi-can-lib { };
+  hi-can-raw = final.callPackage ./hi-can-raw { };
+  hi-can-net = final.callPackage ./hi-can-net { };
 }
