c398fbe550bf9f6c11662ce5d8d7b4722e93e9cf
James N <59348282+RandomSpaceship@users.noreply.github.com>
2024-11-19 02:17:14 +1000

    Configure intersphinx and myst-parser, add and switch to sphinx-immaterial
    - Add python and ros intersphinx mappings
    - Enable select myst extensions
    - Configure sphinx-immaterial
    - Configure sphinx-rtd-theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,24 +6,36 @@ from textwrap import dedent
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
+# Project information
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "Perseus V2"
 copyright = "2024, ROAR Team"
 author = "ROAR Team"
 
-# -- General configuration ---------------------------------------------------
+# may be: "sphinx_immaterial", "sphinx_rtd_theme" or "furo"
+theme = "sphinx_immaterial"
+
+# General configuration
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "breathe", "exhale", "sphinx.ext.githubpages"]
+extensions = [
+    "myst_parser",  # MyST parser - allows for markdown instead of reStructuredText
+    "breathe",
+    "exhale",  # doxygen integration
+    "sphinx.ext.githubpages",  # add .nojekyll file for github pages
+    "sphinx_immaterial.kbd_keys",  # pretty keyboard shortcuts
+    "sphinx.ext.intersphinx",  # link to other projects (specifically ROS and Python)
+]
+if theme == "sphinx_immaterial":
+    extensions.append("sphinx_immaterial")
 
 templates_path = ["_templates"]
 exclude_patterns = []
 
 language = "en"
 
-# -- Extension configuration -------------------------------------------------
+# Extension configuration
 # Set up the breathe extension
 # https://breathe.readthedocs.io/en/stable/quickstart.html
 breathe_projects = {"rover": "../build/doxygen/xml/"}
@@ -34,7 +46,7 @@ exhale_args = {
     # Required arguments
     "containmentFolder": "./generated",
     "rootFileName": "root.rst",
-    "rootFileTitle": "Code Documentation",
+    "rootFileTitle": "Generated Documentation",
     "doxygenStripFromPath": "../../software",
     # Optional arguments
     "createTreeView": True,
@@ -60,18 +72,97 @@ exhale_args = {
 
 # set up MyST parser extension
 # https://myst-parser.readthedocs.io/en/latest/
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "dollarmath",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "strikethrough",
+]
 myst_heading_anchors = 4  # auto-generated heading anchors (slugs)
+suppress_warnings = ["myst.strikethrough"]
 
-# -- Options for markup ------------------------------------------------------
+# intersphinx config
+# this is a good guide: https://docs.readthedocs.io/en/stable/guides/intersphinx.html
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "ros": ("https://docs.ros.org/en/humble", None),
+}
 
+# Options for markup
 primary_domain = "cpp"
 highlight_language = "cpp"
 
-# -- Options for HTML output -------------------------------------------------
+# Options for HTML output
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-use_rtd_theme = False
-html_theme = "sphinx_rtd_theme" if use_rtd_theme else "furo"
+html_theme = theme
 html_static_path = ["_static"]
 
 html_extra_path = ["robots.txt", "README.md"]
+primary = "deep-orange"
+accent = "indigo"
+
+# sphinx-immaterial theme options
+html_theme_options = (
+    {
+        "repo_url": "https://github.com/ROAR-QUTRC/perseus-v2",
+        "repo_name": "Perseus V2",
+        "icon": {
+            "repo": "material/github",
+        },
+        "edit_uri": "edit/main/docs/source/",
+        "features": [
+            # "content.tabs.link", # linked tabs which all switch at once, eg for different languages
+            # "header.autohide", # autohide header on scroll
+            # "navigation.expand", # expand navigation by default (on load)
+            "navigation.instant",  # instant navigation
+            "navigation.sections",  # split navigation into sections (by caption mostly)
+            # "navigation.tabs",
+            # "navigation.tabs.sticky",
+            "navigation.top",  # back to top button
+            # "navigation.tracking", # URL tracks header
+            "search.highlight",  # highlight search results
+            "search.share",  # share search results button
+            # "toc.integrate", # integrate ToC with left navigation
+            "toc.follow",  # ToC active header follows scroll
+            "toc.sticky",  # sticky ToC header
+        ],
+        "palette": [
+            {
+                "media": "(prefers-color-scheme: light)",
+                "scheme": "default",
+                "primary": primary,
+                "accent": accent,
+                "toggle": {
+                    "icon": "material/lightbulb-outline",
+                    "name": "Switch to dark mode",
+                },
+            },
+            {
+                "media": "(prefers-color-scheme: dark)",
+                "scheme": "slate",
+                "primary": primary,
+                "accent": accent,
+                "toggle": {
+                    "icon": "material/lightbulb",
+                    "name": "Switch to light mode",
+                },
+            },
+        ],
+        "globaltoc_collapse": False,
+        "toc_title_is_page_title": True,
+    }
+    if (theme == "sphinx_immaterial")
+    else (
+        {
+            "collapse_navigation": False,
+            "sticky_navigation": True,
+            "navigation_depth": -1,
+        }
+        if theme == "sphinx_rtd_theme"
+        else {}
+    )
+)
