c3eb052dce7c3e6f730ff75bc7f7b720fe267e69
James N <59348282+RandomSpaceship@users.noreply.github.com>
2024-11-21 20:38:58 +1000

    Clean up nix-package.sh - should now handle spaces in filenames and guarantee Git cleanup

--- software/tooling/nix-package.sh
+++ software/tooling/nix-package.sh
@@ -5,12 +5,12 @@ SCRIPT_DIR="$(dirname "$(readlink -f $0)")"
 WORKSPACE_ROOT="$(dirname "$SCRIPT_DIR")"
 cd $WORKSPACE_ROOT
 
-ROS_WS=${WORKSPACE_ROOT}/ros_ws
-OUTPUT_DIR=$ROS_WS/nix-packages
+ROS_WS="$WORKSPACE_ROOT"/ros_ws
+OUTPUT_DIR="$ROS_WS"/nix-packages
 
 echo "Cleaning old packaging files"
-rm -r $OUTPUT_DIR
-mkdir -p $OUTPUT_DIR
+rm -r "$OUTPUT_DIR"
+mkdir -p "$OUTPUT_DIR"
 
 echo "Running ros2nix to generate new packaging files..."
 # need to keep the environment isolated so that direnv doesn't screw with ros2nix.
@@ -19,28 +19,29 @@ echo "Running ros2nix to generate new packaging files..."
 # nix run is the "better" way to do this, but it doesn't allow for complete environment isolation like the shell -i flag.
 nix shell -i github:wentasah/ros2nix --command ros2nix --output-dir="$OUTPUT_DIR" --output-as-nix-pkg-name --no-default --distro humble $(find "$ROS_WS" -name package.xml)
 echo "Formatting generated files"
-cd $OUTPUT_DIR
+cd "$OUTPUT_DIR"
 nix fmt --quiet
 
-if [[ $* == *--no-commit* ]]; then
-    NO_COMMIT=1
+if [[ "$*" == *--no-commit* ]]; then
     echo "Will not commit changes"
+    exit 0
 fi
 
-if [[ -z "$NO_COMMIT" ]] && ! git diff --cached --quiet >/dev/null; then
+if ! git diff --cached --quiet >/dev/null; then
     HAS_GIT_STAGING=1
     echo "Stashing staged changes"
     git stash push -S >/dev/null
 fi
 
-if [[ -z "$NO_COMMIT" ]]; then
-    echo "Staging changes"
-    git add "${OUTPUT_DIR}"
-    echo "Committing changes"
-    git commit -m "$(date +%Y-%m-%dT%H:%M:%S) Nix packaging generation" >/dev/null
-fi
+cleanup() {
+    if [[ -v HAS_GIT_STAGING ]]; then
+        echo "Restoring git stash"
+        git stash pop >/dev/null
+    fi
+}
+trap cleanup EXIT
 
-if [[ -v HAS_GIT_STAGING ]]; then
-    echo "Restoring git stash"
-    git stash pop >/dev/null
-fi
+echo "Staging changes"
+git add "$OUTPUT_DIR"
+echo "Committing changes"
+git commit -m "$(date +%Y-%m-%dT%H:%M:%S) Nix packaging generation" >/dev/null
