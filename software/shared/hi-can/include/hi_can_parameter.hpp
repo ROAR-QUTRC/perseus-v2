56fa77720fc690ee34b811ddbb93a8a964dc86aa
James N <59348282+RandomSpaceship@users.noreply.github.com>
2024-12-12 23:32:15 +1000

    hi-can: WIP parameter group implementation

--- software/shared/hi-can/include/hi_can_parameter.hpp
+++ software/shared/hi-can/include/hi_can_parameter.hpp
@@ -1,13 +1,47 @@
 #include <cstdint>
 
+#include "hi_can.hpp"
+#include "hi_can_address.hpp"
+
 #pragma pack(push, 1)
 namespace hi_can::parameter
 {
+    class ParameterGroup
+    {
+    public:
+        ParameterGroup(addressing::raw_address_t deviceAddress, PacketManager& packetManager)
+            : _deviceAddress(deviceAddress), _packetManager(packetManager) {}
+        ~ParameterGroup()
+        {
+            deregisterParameters();
+        }
+
+        // no copy or move constructors or operators - doesn't make sense
+        ParameterGroup(const ParameterGroup&) = delete;
+        ParameterGroup(ParameterGroup&&) = delete;
+        ParameterGroup& operator=(const ParameterGroup&) = delete;
+        ParameterGroup& operator=(ParameterGroup&&) = delete;
+
+        virtual void registerParameters() = 0;
+        virtual void deregisterParameters() = 0;
+
+    protected:
+        addressing::raw_address_t _deviceAddress;
+        PacketManager& _packetManager;
+    };
     namespace drive
     {
         namespace vesc
         {
+            class VescParameterGroup : public ParameterGroup
+            {
+                void registerParameters() override;
+                void deregisterParameters() override;
+            };
             struct type_1_status
+            {
+            };
+            struct type_2_status
             {
             };
         }
---
+++ software/shared/hi-can/src/hi_can_parameter.cpp
@@ -0,0 +1,12 @@
+#include "hi_can_parameter.hpp"
+
+using namespace hi_can;
+using namespace parameter;
+using namespace drive::vesc;
+
+void VescParameterGroup::registerParameters()
+{
+}
+void VescParameterGroup::deregisterParameters()
+{
+}
