864fc4dd10f9d60604311ef2a203a5f990a30389
James N <59348282+RandomSpaceship@users.noreply.github.com>
2024-12-13 23:49:12 +1000

    scripts(vcan-setup): now loads all needed kernel modules

--- software/scripts/vcan-setup.sh
+++ software/scripts/vcan-setup.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 
 # Set up virtual CAN interface vcan0
+sudo modprobe can
 sudo modprobe vcan
+sudo modprobe can-raw
 sudo ip link add dev vcan0 type vcan
 sudo ip link set up vcan0
