990dc1af16464733b716dbc0c62d91c722a30ed4
James N <59348282+RandomSpaceship@users.noreply.github.com>
2024-12-13 23:55:55 +1000

    hi-can test code

---
+++ software/native/net-can-server/README.md
@@ -0,0 +1,3 @@
+# About
+
+Despite the name, this project currently just serves as a convenient place to test in-development Hi-CAN code.
--- software/native/net-can-server/src/main.cpp
+++ software/native/net-can-server/src/main.cpp
@@ -11,10 +11,69 @@ struct can_data
 
 using namespace hi_can;
 using namespace std;
+using namespace addressing;
+using namespace addressing::legacy;
+using namespace addressing::legacy::drive;
+using namespace parameters::legacy::drive;
+using namespace addressing::legacy::drive::motors;
+using namespace parameters::legacy::drive::motors;
 int main()
 {
     cout << "Pre-initialisation" << endl;
-    auto can = RawCanInterface("any");
+    auto can = RawCanInterface("vcan0");
+    EscParameterGroup groupFL(address_t(
+        SYSTEM_ID,
+        SUBSYSTEM_ID,
+        static_cast<uint8_t>(device::FRONT_LEFT_MOTOR)));
+    EscParameterGroup groupFR(address_t(
+        SYSTEM_ID,
+        SUBSYSTEM_ID,
+        static_cast<uint8_t>(device::FRONT_RIGHT_MOTOR)));
+    EscParameterGroup groupRL(address_t(
+        SYSTEM_ID,
+        SUBSYSTEM_ID,
+        static_cast<uint8_t>(device::REAR_LEFT_MOTOR)));
+    EscParameterGroup groupRR(address_t(
+        SYSTEM_ID,
+        SUBSYSTEM_ID,
+        static_cast<uint8_t>(device::REAR_RIGHT_MOTOR)));
+
+    PacketManager packetManager(can);
+    packetManager.addGroup(groupFL);
+    packetManager.addGroup(groupFR);
+    packetManager.addGroup(groupRL);
+    packetManager.addGroup(groupRR);
+
+    // can.addFilter(filter_t{
+    //     .address = flagged_address_t(),
+    //     .mask = 0,
+    // });
+
+    for (int i = 0; i < 200; i++)
+    {
+        packetManager.handle();
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+    auto& speed = groupFL.getSpeed();
+    speed.enabled = true;
+    speed.direction = motor_direction::FORWARD;
+    speed.speed = 0x77AA;
+    for (int i = 0; i < 200; i++)
+    {
+        packetManager.handle();
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+    auto& status = groupFL.getStatus();
+    can.receive(true);
+    cout << "stat " << status.ready << " " << status.realSpeed << " " << status.realCurrent << endl;
+    cout << "spd " << speed.enabled << " " << (int)speed.direction << " " << speed.speed << endl;
+    speed.direction = motor_direction::REVERSE;
+    for (int i = 0; i < 200; i++)
+    {
+        packetManager.handle();
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
     // can_data d;
     // for (int j = 0; j < 1000; j++)
     // {
@@ -32,13 +91,13 @@ int main()
     //         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     //     }
     // }
-    if (const auto packet = can.receive(true); packet.has_value())
-    {
-        cout << "Received a packet" << endl;
-        cout << "Data: ";
-        auto data = packet->getData();
-        for (size_t i = 0; i < packet->getDataLen(); i++)
-            cout << (int)data[i] << " ";
-        cout << endl;
-    }
+    // if (const auto packet = can.receive(true); packet.has_value())
+    // {
+    //     cout << "Received a packet" << endl;
+    //     cout << "Data: ";
+    //     auto data = packet->getData();
+    //     for (size_t i = 0; i < packet->getDataLen(); i++)
+    //         cout << (int)data[i] << " ";
+    //     cout << endl;
+    // }
 }
