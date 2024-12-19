9be14a7ab1a8a28b7aaab60113595df91f174b92
James N <59348282+RandomSpaceship@users.noreply.github.com>
2024-12-19 15:32:44 +1000

    scripts: Fix vcan setup
    Now loads *all* required modules, not just vcan.
    
    Also will no longer fail on a single command, to allow it to finish partially complete setup

--- software/scripts/vcan-setup.sh
+++ software/scripts/vcan-setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
 # Set up virtual CAN interface vcan0
+sudo modprobe can
 sudo modprobe vcan
+sudo modprobe can-raw
 sudo ip link add dev vcan0 type vcan
 sudo ip link set up vcan0
