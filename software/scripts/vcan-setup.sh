903620ba8372ced225cc6df4f81176c5514bb37e
James N <59348282+RandomSpaceship@users.noreply.github.com>
2024-12-20 18:13:49 +1000

    scripts: Add bus name parameter to vcan-setup

--- software/scripts/vcan-setup.sh
+++ software/scripts/vcan-setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+CAN_IF="${1:-vcan0}"
+
 # Set up virtual CAN interface vcan0
 sudo modprobe can
 sudo modprobe vcan
 sudo modprobe can-raw
-sudo ip link add dev vcan0 type vcan
-sudo ip link set up vcan0
+sudo ip link add dev "${CAN_IF}" type vcan
+sudo ip link set up "${CAN_IF}"
