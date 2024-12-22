aad6d8ec5bef1344bd17e6ade70eca6fabea8cdc
James N <59348282+RandomSpaceship@users.noreply.github.com>
2024-12-22 19:49:19 +1000

    perseus_sensors(m2m2): Add package + empty node

---
+++ software/ros_ws/src/perseus_sensors/src/m2m2_lidar/m2m2_lidar.cpp
@@ -0,0 +1,12 @@
+#include "m2m2_lidar.hpp"
+
+#include <cstdio>
+
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv;
+
+    printf("hello world sensors package\n");
+    return 0;
+}
