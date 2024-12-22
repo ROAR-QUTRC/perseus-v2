d4d5468082522660b6b462ae43af619fb9982a29
James N <59348282+RandomSpaceship@users.noreply.github.com>
2024-12-22 19:38:15 +1000

    cmake(templates): Remove ROS exe install versioning
    - Despite symlinking the "real" executable, ros2 run can't see the node, so versioning has to be removed.

--- software/templates/cmake_snippets/71-ros_install_exe.cmake
+++ software/templates/cmake_snippets/71-ros_install_exe.cmake
@@ -1,4 +1,2 @@
 # INSTALL (ROS Executables)
 install(TARGETS ${PROJECT_NAME} DESTINATION lib/${PROJECT_NAME})
-# Versioning
-set_target_properties(${PROJECT_NAME} PROPERTIES VERSION ${PROJECT_VERSION})
