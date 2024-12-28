a2e1fb9d5297385d6769474ba0d371885c1a7370
DingoOz <dingo.australia@gmail.com>
2024-12-28 13:47:50 +1000

    ros_ws(nix): +1000:2024-12-28T13:47:50 Nix packaging generation

--- software/ros_ws/nix-packages/perseus-simulation.nix
+++ software/ros_ws/nix-packages/perseus-simulation.nix
@@ -4,7 +4,7 @@
   buildRosPackage,
   ament-cmake,
   backward-ros,
-  gazebo-ros,
+  gazebo-ros-pkgs,
   perseus-description,
   perseus-hardware,
   rclcpp,
@@ -19,7 +19,7 @@ buildRosPackage rec {
   buildInputs = [ ament-cmake ];
   propagatedBuildInputs = [
     backward-ros
-    gazebo-ros
+    gazebo-ros-pkgs
     perseus-description
     perseus-hardware
     rclcpp
