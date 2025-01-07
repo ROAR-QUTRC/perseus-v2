39ee102ae8cc0869c6662e66e07fc99d03980ed0
OMEN44 <huonswales@gmail.com>
2025-01-07 20:08:54 +1000

    web_ui: Initial commit

---
+++ software/web_ui/src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts" module>
+	// Dynamically import all widgets
+	const widgets = import.meta.glob('../lib/widgets/**.svelte', { eager: true });
+	availableWidgets.length = 0;
+	for (const widget in widgets) {
+		availableWidgets.push({
+			name: (widgets[widget] as any).name,
+			settings: (widgets[widget] as any).settings,
+			component: (widgets[widget] as any).default
+		});
+	}
+</script>
+
+<script lang="ts">
+	import { ModeWatcher } from 'mode-watcher';
+	import '../app.css';
+	import { Toaster } from '$lib/components/ui/sonner';
+	import { onMount } from 'svelte';
+	import { connect, ros } from '$lib/scripts/ros.svelte';
+	import { availableWidgets } from '$lib/scripts/state.svelte';
+	import { localStore } from '$lib/scripts/localStore.svelte';
+	let { children } = $props();
+
+	// automatically connect and disconnect from ROS-bridge
+	onMount(() => {
+		connect(localStore('rosAddress', 'localhost').value);
+		// This really should be it's own function (the return value is the onUnMount function)
+		return () => {
+			if (ros.value) ros.value.close();
+		};
+	});
+</script>
+
+<ModeWatcher defaultMode={'dark'} />
+<Toaster position="bottom-left" />
+{@render children()}
---
+++ software/web_ui/src/routes/api/ros/domain/+server.ts
@@ -0,0 +1,31 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import { exec } from 'child_process';
+
+export const POST: RequestHandler = async ({ request }) => {
+	const { comainId } = await request.json();
+
+	exec(`export ROS_DOMAIN_ID=${comainId}`, (error, stdout, stderr) => {
+		if (error) {
+			console.error(`exec error: ${error}`);
+			return;
+		}
+		if (stderr) {
+			console.error(`stderr: ${stderr}`);
+			return;
+		}
+	});
+
+	return new Response(JSON.stringify(comainId));
+};
+
+export const GET: RequestHandler = async (req) => {
+	return new Promise((resolve, reject) => {
+		exec('hostname -I', (error, stdout, stderr) => {
+			resolve(
+				new Response(
+					JSON.stringify(stdout.length === 1 ? 'localhost' : stdout.substring(0, stdout.length - 2))
+				)
+			);
+		});
+	});
+};
