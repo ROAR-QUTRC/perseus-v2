2ed4206413430b245a6731bd44a06de8364e78d6
OMEN44 <OMEN44@users.noreply.github.com>
2025-01-10 07:43:24 +0000

    chore: Format and lint

--- software/web_ui/src/routes/api/widget/resources/+server.ts
+++ software/web_ui/src/routes/api/widget/resources/+server.ts
@@ -1,38 +1,43 @@
-import type { RequestHandler } from '@sveltejs/kit';
-import { exec } from 'child_process';
-import os from 'os';
-import { cpuUsage } from 'process';
+import type { RequestHandler } from "@sveltejs/kit";
+import { exec } from "child_process";
+import os from "os";
+import { cpuUsage } from "process";
 
 let prevTimes: number[] = [];
 let prevIdle: number = 0;
 let prevActive: number = 0;
 
 export const GET: RequestHandler = async (req) => {
-	return new Promise((resolve, reject) => {
-		exec('cat /proc/stat', (error, stdout, stderr) => {
-			const times: number[] = stdout
-				.split('\n')[0]
-				.replace('cpu  ', '') // the two traliing spaces look weird and may break things
-				.split(' ')
-				.map((time) => parseInt(time));
+  return new Promise((resolve, reject) => {
+    exec("cat /proc/stat", (error, stdout, stderr) => {
+      const times: number[] = stdout
+        .split("\n")[0]
+        .replace("cpu  ", "") // the two traliing spaces look weird and may break things
+        .split(" ")
+        .map((time) => parseInt(time));
 
-			// algorithm refernce mainly from: https://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
-			// check the man page for /proc/stat for the order of the values
-			const idle: number = times[3] + times[4]; // calcualte idle time
-			const active: number = times[0] + times[1] + times[2] + times[5] + times[6] + times[7]; // calcuate active time
+      // algorithm reference mainly from: https://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
+      // check the man page for /proc/stat for the order of the values
+      const idle: number = times[3] + times[4]; // calculate idle time
+      const active: number =
+        times[0] + times[1] + times[2] + times[5] + times[6] + times[7]; // calculate active time
 
-			// calculate the delta between the current and previous values
-			const idleDelta: number = idle - prevIdle;
-			const activeDelta: number = active - prevActive;
+      // calculate the delta between the current and previous values
+      const idleDelta: number = idle - prevIdle;
+      const activeDelta: number = active - prevActive;
 
-			// calculate the cpu usage
-			const cpuUsage: number = activeDelta / (activeDelta + idleDelta);
+      // calculate the cpu usage
+      const cpuUsage: number = activeDelta / (activeDelta + idleDelta);
 
-			prevTimes = times;
-			prevIdle = idle;
-			prevActive = active;
+      prevTimes = times;
+      prevIdle = idle;
+      prevActive = active;
 
-			resolve(new Response(JSON.stringify({ cpuUsage: Math.round(cpuUsage * 10000) / 100 })));
-		});
-	});
+      resolve(
+        new Response(
+          JSON.stringify({ cpuUsage: Math.round(cpuUsage * 10000) / 100 }),
+        ),
+      );
+    });
+  });
 };
