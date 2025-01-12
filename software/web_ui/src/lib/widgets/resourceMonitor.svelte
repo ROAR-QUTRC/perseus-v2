aa691dd821212522687caa0a5ea60c63cd355f30
OMEN44 <huonswales@gmail.com>
2025-01-12 14:38:59 +1000

    Finshed resrouce monitor widget

--- software/web_ui/src/lib/widgets/resourceMonitor.svelte
+++ software/web_ui/src/lib/widgets/resourceMonitor.svelte
@@ -16,18 +16,6 @@
 					type: 'number',
 					description: 'The number of data points to store in each chart',
 					value: '25'
-				},
-				networkInterface: {
-					type: 'select',
-					description: 'Network interface to monitor',
-					options: []
-				},
-				reloadNetworkInterfaces: {
-					type: 'button',
-					action: () => {
-						// updated network interfaces
-						return 'Not implemented';
-					}
 				}
 			},
 			memory: {
@@ -46,6 +34,18 @@
 					type: 'switch',
 					description: 'Calculate memory using 1024 not 1000'
 				}
+			},
+			network: {
+				networkInterface: {
+					type: 'select',
+					description: 'Network interface to monitor',
+					options: []
+				},
+				setUsageScale: {
+					type: 'number',
+					description: 'Set the maximum value on the y axis in KB/s',
+					value: '5000'
+				}
 			}
 		}
 	});
@@ -58,6 +58,18 @@
 	$effect(() => {
 		if (Number(settings.groups.general.refreshRate.value) < 500) {
 			settings.groups.general.refreshRate.value = '500';
+		}
+	});
+
+	// update the y axis of the network chart
+	$effect(() => {
+		const maxValue = Number(settings.groups.network.setUsageScale.value);
+		if (netChart !== undefined) {
+			netChart.options.scales!.y!.max = maxValue;
+			netChart.data.datasets[2].data = Array(
+				Number(settings.groups.general.dataPointsToStore.value)
+			).fill(maxValue * 0.8);
+			netChart.update();
 		}
 	});
 
@@ -93,13 +105,24 @@
 			memChart.data.datasets[i].data = memChart.data.datasets[i].data.slice(-limit);
 		}
 
+		netChart.data.labels = netChart.data.labels?.slice(-limit);
+		for (let i = 0; i < 2; i++) {
+			netChart.data.datasets[i].data = netChart.data.datasets[i].data.slice(-limit);
+		}
+
 		// fetch data
-		fetch('/api/widget/resources', { method: 'GET' }).then((res) => {
+		fetch('/api/widget/resources', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ networkIf: settings.groups.network.networkInterface.value })
+		}).then((res) => {
 			if (res.ok) {
 				res.json().then((d) => {
 					// CPU
 					cpuChart.data.datasets[0].data.push(JSON.parse(d[0]).cpuUsage);
-					cpuChart.data.labels?.push(new Date().toLocaleTimeString('en-GB'));
+					cpuChart.data.labels?.push('');
 					cpuChart.update();
 
 					// Memory
@@ -107,7 +130,7 @@
 						Number(JSON.parse(d[1]).total[0]),
 						settings.groups.memory.sizeFormat.value!
 					);
-					memChart.data.labels?.push(new Date().toLocaleTimeString('en-GB'));
+					memChart.data.labels?.push('');
 
 					memChart.data.datasets[0].data.push(
 						convertBytes(
@@ -127,6 +150,27 @@
 					memChart.update();
 
 					// Network
+					netChart.data.labels?.push('');
+					netChart.data.datasets[0].data.push(
+						convertBytes(
+							Number(JSON.parse(d[2]).tx) *
+								(1 / (Number(settings.groups.general.refreshRate.value) / 1000)),
+							'KB'
+						)
+					);
+					netChart.data.datasets[1].data.push(
+						convertBytes(
+							Number(JSON.parse(d[2]).rx) *
+								(1 / (Number(settings.groups.general.refreshRate.value) / 1000)),
+							'KB'
+						)
+					);
+					if (netChart.data.datasets[2].data.length < netChart.data.labels!.length) {
+						netChart.data.datasets[2].data.push(
+							Number(settings.groups.network.setUsageScale.value) * 0.8
+						);
+					}
+					netChart.update();
 				});
 			}
 		});
@@ -137,6 +181,16 @@
 	};
 
 	onMount(() => {
+		fetch('/api/widget/resources', { method: 'GET' }).then(async (res) => {
+			if (res.ok) {
+				const data: string[] = await res.json();
+				settings.groups.network.networkInterface.options = data.map((networkIf) => {
+					return { value: networkIf, label: networkIf };
+				});
+				settings.groups.network.networkInterface.value = data[0];
+			}
+		});
+
 		cpuChart = new Chart(cpuCanvas?.getContext('2d')!, {
 			data: {
 				labels: [],
@@ -185,13 +239,22 @@
 		netChart = new Chart(netCanvas?.getContext('2d')!, {
 			data: {
 				labels: [],
-				datasets: [{ label: 'Network usage', data: [] }]
+				datasets: [
+					{ label: 'Upload', data: [] },
+					{ label: 'Download', data: [] },
+					{
+						label: '80% marker',
+						data: [],
+						pointStyle: false
+					}
+				]
 			},
 			type: 'line',
 			options: {
 				scales: {
 					y: {
 						beginAtZero: true,
+						max: Number(settings.groups.network.setUsageScale.value),
 						title: {
 							display: true,
 							text: 'Network Usage (KB/s)'
@@ -201,6 +264,11 @@
 				animation: false
 			}
 		});
+
+		netChart.data.datasets[2].data = [
+			Number(settings.groups.network.setUsageScale.value) * 0.8,
+			Number(settings.groups.network.setUsageScale.value) * 0.8
+		];
 
 		// start intervals
 		updateData();
@@ -214,8 +282,14 @@
 	});
 </script>
 
-<div class="flex">
-	<canvas bind:this={cpuCanvas}></canvas>
-	<canvas bind:this={memCanvas}></canvas>
-	<canvas bind:this={netCanvas}></canvas>
+<div class="flex h-full w-full flex-wrap">
+	<div class="">
+		<canvas bind:this={cpuCanvas}></canvas>
+	</div>
+	<div class="">
+		<canvas bind:this={memCanvas}></canvas>
+	</div>
+	<div class="">
+		<canvas bind:this={netCanvas}></canvas>
+	</div>
 </div>
--- software/web_ui/src/routes/api/widget/resources/+server.ts
+++ software/web_ui/src/routes/api/widget/resources/+server.ts
@@ -1,59 +1,93 @@
-import type { RequestHandler } from "@sveltejs/kit";
-import { exec } from "child_process";
+import type { RequestHandler } from '@sveltejs/kit';
+import { exec } from 'child_process';
 
 let prevIdle: number = 0;
 let prevActive: number = 0;
 
-export const GET: RequestHandler = async (req) => {
-  let cpuPromise = new Promise((resolve, reject) => {
-    exec("cat /proc/stat && free -t", (error, stdout, stderr) => {
-      const times: number[] = stdout
-        .split("\n")[0]
-        .replace("cpu  ", "") // the two traliing spaces look weird and may break things
-        .split(" ")
-        .map((time) => parseInt(time));
+let prevRxBytes: number = 0;
+let prevTxBytes: number = 0;
 
-      // algorithm reference mainly from: https://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
-      // check the man page for /proc/stat for the order of the values
-      const idle: number = times[3] + times[4]; // calculate idle time
-      const active: number =
-        times[0] + times[1] + times[2] + times[5] + times[6] + times[7]; // calculate active time
+export const POST: RequestHandler = async ({ request }) => {
+	let cpuPromise = new Promise((resolve, reject) => {
+		exec('cat /proc/stat && free -t', (error, stdout, stderr) => {
+			const times: number[] = stdout
+				.split('\n')[0]
+				.replace('cpu  ', '') // the two traliing spaces look weird and may break things
+				.split(' ')
+				.map((time) => parseInt(time));
 
-      // calculate the delta between the current and previous values
-      const idleDelta: number = idle - prevIdle;
-      const activeDelta: number = active - prevActive;
+			// algorithm reference mainly from: https://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
+			// check the man page for /proc/stat for the order of the values
+			const idle: number = times[3] + times[4]; // calculate idle time
+			const active: number = times[0] + times[1] + times[2] + times[5] + times[6] + times[7]; // calculate active time
 
-      // calculate the cpu usage
-      const cpuUsage: number = activeDelta / (activeDelta + idleDelta);
+			// calculate the delta between the current and previous values
+			const idleDelta: number = idle - prevIdle;
+			const activeDelta: number = active - prevActive;
 
-      prevIdle = idle;
-      prevActive = active;
+			// calculate the cpu usage
+			const cpuUsage: number = activeDelta / (activeDelta + idleDelta);
 
-      resolve(JSON.stringify({ cpuUsage: Math.round(cpuUsage * 10000) / 100 }));
-    });
-  });
+			prevIdle = idle;
+			prevActive = active;
 
-  let memPromise = new Promise((resolve, reject) => {
-    exec("free -tb", (error, stdout, stderr) => {
-      // For both totals and swap:
-      // 0: total
-      // 1: used
-      // 2: free
-      // all values in bytes
-      const totals: string[] = stdout
-        .split("\n")[3]
-        .split(" ")
-        .filter((info) => info !== "" && info !== "Total:");
-      const swap: string[] = stdout
-        .split("\n")[2]
-        .split(" ")
-        .filter((info) => info !== "" && info !== "Swap:");
+			resolve(JSON.stringify({ cpuUsage: Math.round(cpuUsage * 10000) / 100 }));
+		});
+	});
 
-      resolve(JSON.stringify({ total: totals, swap: swap }));
-    });
-  });
+	let memPromise = new Promise((resolve, reject) => {
+		exec('free -tb', (error, stdout, stderr) => {
+			// For both totals and swap:
+			// 0: total
+			// 1: used
+			// 2: free
+			// all values in bytes
+			const totals: string[] = stdout
+				.split('\n')[3]
+				.split(' ')
+				.filter((info) => info !== '' && info !== 'Total:');
+			const swap: string[] = stdout
+				.split('\n')[2]
+				.split(' ')
+				.filter((info) => info !== '' && info !== 'Swap:');
 
-  return new Response(
-    JSON.stringify(await Promise.all([cpuPromise, memPromise])),
-  );
+			resolve(JSON.stringify({ total: totals, swap: swap }));
+		});
+	});
+
+	let netPromise = new Promise(async (resolve, reject) => {
+		const { networkIf } = await request.json();
+		if (networkIf === undefined) {
+			resolve(JSON.stringify({ rx: 0, tx: 0 }));
+			return;
+		}
+		exec(
+			`cat /sys/class/net/${networkIf}/statistics/rx_bytes && cat /sys/class/net/${networkIf}/statistics/tx_bytes`,
+			(error, stdout) => {
+				const rxBytes: number = parseInt(stdout.split('\n')[0]);
+				const txBytes: number = parseInt(stdout.split('\n')[1]);
+
+				const rxDelta: number = rxBytes - prevRxBytes;
+				const txDelta: number = txBytes - prevTxBytes;
+				prevRxBytes = rxBytes;
+				prevTxBytes = txBytes;
+
+				resolve(JSON.stringify({ rx: rxDelta, tx: txDelta }));
+			}
+		);
+	});
+
+	return new Response(JSON.stringify(await Promise.all([cpuPromise, memPromise, netPromise])));
+};
+
+export const GET: RequestHandler = async () => {
+	return new Response(
+		JSON.stringify(
+			await new Promise((resolve, reject) => {
+				exec('ls /sys/class/net', (error, stdout, stderr) => {
+					resolve(stdout.split('\n').filter((iface) => iface !== ''));
+				});
+			})
+		)
+	);
 };
