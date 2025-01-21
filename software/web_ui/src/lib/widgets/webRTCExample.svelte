7f02776846d42e47872651d3704eb4f8cac174cb
OMEN44 <huonswales@gmail.com>
2025-01-21 16:19:36 +1000

    revice raw jpeg stream over udp

--- software/web_ui/src/lib/widgets/webRTCExample.svelte
+++ software/web_ui/src/lib/widgets/webRTCExample.svelte
@@ -2,7 +2,7 @@
 	// This is to expose the widget settings to the panel. Code in here will only run once when the widget is first loaded.
 	import type { WidgetSettingsType } from '$lib/scripts/state.svelte';
 
-	export const name = 'Video stream';
+	export const name = 'WebRTC example';
 
 	export const settings: WidgetSettingsType = $state<WidgetSettingsType>({
 		groups: {}
@@ -10,15 +10,17 @@
 </script>
 
 <script lang="ts">
-	import { ros } from '$lib/scripts/ros.svelte'; // ROSLIBJS docs here: https://robotwebtools.github.io/roslibjs/Service.html
-	import ROSLIB from 'roslib';
-	import { onMount } from 'svelte';
+	/*
+	gstreamer webrtcsink example:
 
-	let cameraTopic = new ROSLIB.Topic({
-		ros: ros.value!,
-		name: '/image_raw/compressed',
-		messageType: 'sensor_msgs/msg/CompressedImage'
-	});
+	run these commands in seperate terminals:
+	~$ 											gst-launch-1.0 webrtcsink name=ws meta="meta,name=gst-stream" libcamerasrc ! ws.
+	~/gst-plugins-rs/net/webrtc/signalling $    WEBRTCSINK_SIGNALLING_SERVER_LOG=debug cargo run --bin gst-webrtc-signalling-server -- --port 8443
+	~/gst-plugins-rs/net/webrtc/gstwebrtc-api $ npm start
+	*/
+
+	import { onMount } from 'svelte';
+	import { io, Socket } from 'socket.io-client';
 
 	function dataURItoBlob(imageFormat: string, dataURI: string) {
 		// convert base64 to raw binary data held in a string
@@ -39,10 +41,11 @@
 
 	let canvas = $state<HTMLCanvasElement>();
 	onMount(() => {
+		const socket: Socket = io();
 		const ctx = canvas!.getContext('2d');
-		cameraTopic.subscribe((message: any) => {
+		socket.on('video', async (message: { raw: any; info: any }) => {
 			if (canvas !== undefined && ctx !== null) {
-				createImageBitmap(dataURItoBlob('image/jpeg', message.data)).then((data) => {
+				createImageBitmap(dataURItoBlob('image/jpeg', message.raw)).then((data) => {
 					canvas!.width = data.width;
 					canvas!.height = data.height;
 					ctx.drawImage(data, 0, 0);
@@ -57,14 +60,12 @@
 					// ctx.fill();
 				});
 			}
-
-			// cameraTopic.unsubscribe();
 		});
 
-		return () => {
-			cameraTopic.unsubscribe();
-		};
+		return () => {};
 	});
 </script>
+
+<!-- <img src={image} alt="img" /> -->
 
 <canvas bind:this={canvas}></canvas>
--- software/web_ui/vite.config.ts
+++ software/web_ui/vite.config.ts
@@ -4,7 +4,26 @@ import { defineConfig, type ViteDevServer } from 'vite';
 import { Server } from 'socket.io';
 import { resourceMonitor } from './src/server/scripts/resourceMonitorSocket';
 
+import dgram from 'node:dgram';
+
 let clientCount: number = 0;
+
+function dataURItoBlob(imageFormat: string, data: any) {
+	console.log('Byte string: ', data);
+	console.log('Byte string length: ', data.length);
+
+	// write the bytes of the string to an ArrayBuffer
+	var ab = new ArrayBuffer(data.length);
+	console.log('ArrayBuffer 1: ', ab);
+	var ia = new Uint8Array(ab);
+
+	console.log('ArrayBuffer 2: ', ab);
+
+	// write the ArrayBuffer to a blob, and you're done
+	var blob = new Blob([ab], { type: imageFormat });
+	console.log('Blob: ', blob);
+	return blob;
+}
 
 export const webSocketServer = {
 	name: 'webSocketServer',
@@ -26,6 +45,27 @@ export const webSocketServer = {
 		});
 
 		resourceMonitor(io);
+
+		// UDP video streams
+		const udpServer = dgram.createSocket('udp4');
+
+		udpServer.on('error', (err) => {
+			console.log(`server error:\n${err.stack}`);
+		});
+
+		udpServer.on('message', (msg, rinfo) => {
+			// console.log(`server got: ${msg} from`, rinfo);
+			// command: gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg, width=320, height=240, framerate=30/1 ! udpsink host=0.0.0.0 port=8123
+			// size: 22984
+			io.emit(`video`, { raw: msg.toString('base64'), info: rinfo });
+		});
+
+		udpServer.on('listening', () => {
+			const address = udpServer.address();
+			console.log(`server listening ${address.address}:${address.port}`);
+		});
+
+		udpServer.bind(8123);
 	}
 };
 
