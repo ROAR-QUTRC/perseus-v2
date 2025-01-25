4c4f12e6c65ad30ec0f973f87233bd3a4d1a5f57
DingoOz <dingo.australia@gmail.com>
2025-01-25 21:41:19 +1000

    autonomy: split launch so perseus bringup launch is used

--- software/ros_ws/src/autonomy/launch/mapping_using_slam_toolbox.launch.py
+++ software/ros_ws/src/autonomy/launch/mapping_using_slam_toolbox.launch.py
@@ -1,40 +1,21 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
-from launch.substitutions import (
-    LaunchConfiguration,
-    PathJoinSubstitution,
-    Command,
-    FindExecutable,
+from launch.actions import (
+    DeclareLaunchArgument,
+    ExecuteProcess,
+    IncludeLaunchDescription,
 )
-from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    # ARGUMENTS
-    use_mock_hardware = LaunchConfiguration("use_mock_hardware")
-    use_legacy_hardware = LaunchConfiguration("use_legacy_hardware")
-    can_bus = LaunchConfiguration("can_bus")
+    # Arguments
     use_sim_time = LaunchConfiguration("use_sim_time")
     slam_params_file = LaunchConfiguration("slam_params_file")
 
-    arguments = [
-        DeclareLaunchArgument(
-            "use_mock_hardware",
-            default_value="False",
-            description="Use mock hardware components which mirror commands to state interfaces",
-        ),
-        DeclareLaunchArgument(
-            "use_legacy_hardware",
-            default_value="True",
-            description="Use legacy hardware (MCB) interfaces",
-        ),
-        DeclareLaunchArgument(
-            "can_bus",
-            default_value="can0",
-            description="CAN bus to use for hardware communications",
-        ),
+    declare_args = [
         DeclareLaunchArgument(
             "use_sim_time",
             default_value="false",
@@ -49,86 +30,21 @@ def generate_launch_description():
         ),
     ]
 
-    # CONFIG FILES
-    controller_config = PathJoinSubstitution(
-        [FindPackageShare("perseus"), "config", "perseus_controllers.yaml"]
-    )
-
-    # XACRO FILES
-    robot_description_xacro = PathJoinSubstitution(
-        [FindPackageShare("perseus"), "urdf", "perseus.urdf.xacro"]
-    )
-    robot_description_content = ParameterValue(
-        Command(
+    # Include robot bringup from perseus package
+    perseus_bringup = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
             [
-                FindExecutable(name="xacro"),
-                # pass through all the arguments
-                " ",
-                robot_description_xacro,
-                " ",
-                "use_mock_hardware:=",
-                use_mock_hardware,
-                " ",
-                "use_legacy_hardware:=",
-                use_legacy_hardware,
-                " ",
-                "can_bus:=",
-                can_bus,
+                PathJoinSubstitution(
+                    [FindPackageShare("perseus"), "launch", "perseus.launch.py"]
+                )
             ]
         ),
-        value_type=str,
+        launch_arguments={
+            "use_sim_time": use_sim_time,
+        }.items(),
     )
 
-    # NODES
-    controller_manager = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[controller_config],
-        output="both",  # output to both screen and log file
-        remappings=[
-            ("~/robot_description", "/robot_description"),
-            ("/perseus_base_controller/cmd_vel", "/cmd_vel"),
-        ],
-    )
-
-    robot_state_publisher = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="both",
-        parameters=[
-            {
-                "robot_description": robot_description_content,
-                "use_sim_time": use_sim_time,
-                "publish_frequency": 30.0,
-                "frame_prefix": "",  # Ensure no frame prefix is added
-            }
-        ],
-        remappings=[
-            ("/tf", "tf"),
-            ("/tf_static", "tf_static"),
-        ],
-    )
-
-    joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            "joint_state_broadcaster",
-            "--controller-manager",
-            "/controller_manager",
-        ],
-    )
-
-    base_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            "perseus_base_controller",
-            "--controller-manager",
-            "/controller_manager",
-        ],
-    )
-
+    # SLAM nodes
     static_tf_publisher = Node(
         package="tf2_ros",
         executable="static_transform_publisher",
@@ -164,7 +80,7 @@ def generate_launch_description():
     )
 
     # RViz with nixGL support
-    rviz_config_dir = PathJoinSubstitution(
+    rviz_config = PathJoinSubstitution(
         [FindPackageShare("autonomy"), "rviz", "perseus_slam.rviz"]
     )
     rviz = ExecuteProcess(
@@ -176,7 +92,7 @@ def generate_launch_description():
             "--",
             "rviz2",
             "-d",
-            rviz_config_dir,
+            rviz_config,
         ],
         output="screen",
         additional_env={
@@ -190,17 +106,10 @@ def generate_launch_description():
     )
 
     nodes = [
-        robot_state_publisher,
-        controller_manager,
-        base_controller_spawner,
-        joint_state_broadcaster_spawner,
+        perseus_bringup,
         static_tf_publisher,
         slam_toolbox,
         rviz,
     ]
 
-    # EVENT HANDLERS
-    handlers = []
-
-    # Return the launch description
-    return LaunchDescription(arguments + nodes + handlers)
+    return LaunchDescription(declare_args + nodes)
