f136a0fa4ff89a1a245cdd48ec675af212ebc6b3
DanB <jdbretherton12@gmail.com>
2025-01-30 00:19:10 +1000

    ros_ws(nix): +1000:2025-01-30T00:19:10 Nix packaging generation

--- software/ros_ws/nix-packages/overlay.nix
+++ software/ros_ws/nix-packages/overlay.nix
@@ -2,9 +2,9 @@ final: prev: {
   autonomy = final.callPackage ./autonomy.nix { };
   input-devices = final.callPackage ./input-devices.nix { };
   perseus = final.callPackage ./perseus.nix { };
-  perseus-behavior = final.callPackage ./perseus-behavior.nix { };
   perseus-description = final.callPackage ./perseus-description.nix { };
   perseus-hardware = final.callPackage ./perseus-hardware.nix { };
+  perseus-navigator = final.callPackage ./perseus-navigator.nix { };
   perseus-sensors = final.callPackage ./perseus-sensors.nix { };
   perseus-simulation = final.callPackage ./perseus-simulation.nix { };
 }
