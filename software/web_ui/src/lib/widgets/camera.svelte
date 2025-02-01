619acdae6a352add8f1d23ceedd4803f711ee535
OMEN44 <huonswales@gmail.com>
2025-02-01 12:40:21 +1000

    Web RTC video works

--- software/web_ui/src/lib/widgets/camera.svelte
+++ software/web_ui/src/lib/widgets/camera.svelte
@@ -2,7 +2,7 @@
 	// This is to expose the widget settings to the panel. Code in here will only run once when the widget is first loaded.
 	import type { WidgetSettingsType } from '$lib/scripts/state.svelte';
 
-	export const name = 'Video stream';
+	export const name = 'Video - ROS2';
 
 	export const settings: WidgetSettingsType = $state<WidgetSettingsType>({
 		groups: {}
