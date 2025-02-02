1d41ed67fe237a21b969ff532a7e60b4587d7698
James N <59348282+RandomSpaceship@users.noreply.github.com>
2025-02-02 23:45:28 +1000

    feat: Add bucket driver node
    Takes `Actuator` messages and converts them to CAN packets, with fail-safe

--- software/ros_ws/src/perseus_payloads/include/bucket_driver/bucket_driver.hpp
+++ software/ros_ws/src/perseus_payloads/include/bucket_driver/bucket_driver.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <chrono>
 #include <actuator_msgs/msg/actuators.hpp>
+#include <hi_can_raw.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 class BucketDriver : public rclcpp::Node
@@ -9,4 +11,16 @@ public:
     explicit BucketDriver(const rclcpp::NodeOptions& options = rclcpp::NodeOptions());
 
 private:
+    void _actuatorCallback(const actuator_msgs::msg::Actuators::SharedPtr msg);
+    void _timeoutCallback();
+
+    void _writeActuators(double lift, double tilt, double jaws, double rotate, bool magnet);
+
+    constexpr static auto ACTUATOR_TIMEOUT = std::chrono::milliseconds(300);
+
+    bool _timedOut = false;
+    hi_can::RawCanInterface _canInterface;
+
+    rclcpp::Subscription<actuator_msgs::msg::Actuators>::SharedPtr _actuatorSubscription;
+    rclcpp::TimerBase::SharedPtr _killTimer;
 };
