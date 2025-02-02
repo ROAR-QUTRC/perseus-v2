e55d6b9232618bd606c500ff6744a44765f1083f
GR3Y-SCALE <62605251+GR3Y-SCALE@users.noreply.github.com>
2025-02-02 12:23:23 +0000

    chore: Format and lint

--- firmware/light-tower/src/main.cpp
+++ firmware/light-tower/src/main.cpp
@@ -4,17 +4,21 @@
 #define DATA_PIN 12
 #define NUM_LEDS 60
 CRGB leds[NUM_LEDS];
- 
-void setup() { 
-    FastLED.addLeds<WS2811, DATA_PIN, BRG>(leds, NUM_LEDS); //BRG is the order of this specific strip, usually they are RGB.
+
+void setup()
+{
+    FastLED.addLeds<WS2811, DATA_PIN, BRG>(leds, NUM_LEDS);  // BRG is the order of this specific strip, usually they are RGB.
 }
- 
-void loop() { 
-  // Turn the LED on, then pause 
-  for (int i = 0; i <= 32; i++) { // each band contains 8 segments of LEDs, 0 is first, 44 is last (top).
-    leds[i] = CRGB(255,255,255);
-  }
-  for (int i = 32; i <= 44; i++) {
-    leds[i] = CRGB(255,255,255);
-  }
+
+void loop()
+{
+    // Turn the LED on, then pause
+    for (int i = 0; i <= 32; i++)
+    {  // each band contains 8 segments of LEDs, 0 is first, 44 is last (top).
+        leds[i] = CRGB(255, 255, 255);
+    }
+    for (int i = 32; i <= 44; i++)
+    {
+        leds[i] = CRGB(255, 255, 255);
+    }
 }
