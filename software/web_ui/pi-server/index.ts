15c80d6ff9763166353c582bf2b741d21de9272e
OMEN44 <huonswales@gmail.com>
2025-02-10 14:54:03 +1000

    updated the way the gstreamer command is executed

--- software/web_ui/pi-server/index.ts
+++ software/web_ui/pi-server/index.ts
@@ -1,25 +1,32 @@
 import { io, Socket } from 'socket.io-client';
 import config from './config.json';
-import { spawn } from 'child_process';
+import { exec, spawn } from 'child_process';
 
 const socket: Socket = io(`http://${config.webServer.ip}:${config.webServer.port}`, {
 	reconnection: true,
 	reconnectionDelay: 1000
 });
 
-let gstreamerInstance = spawn('gst-launch-1.0', [
-	'webrtcsink',
-	'run-signalling-server=true',
-	'stun-server=NULL',
-	'name=ws',
-	'videtestsrc',
-	'!',
-	'ws.'
-]);
+exec(
+	'gst-launch-1.0 webrtcsink run-signalling-server=true stun-server=NULL name=ws videtestsrc ! ws.',
+	(error, stdout, stderr) => {
+		console.log(`stdout: ${stdout}`);
+	}
+);
 
-gstreamerInstance.stdout.on('data', (data) => {
-	console.log(`stdout: ${data}`);
-});
+// let gstreamerInstance = spawn('gst-launch-1.0', [
+// 	'webrtcsink',
+// 	'run-signalling-server=true',
+// 	'stun-server=NULL',
+// 	'name=ws',
+// 	'videtestsrc',
+// 	'!',
+// 	'ws.'
+// ]);
+
+// gstreamerInstance.stdout.on('data', (data) => {
+// 	console.log(`stdout: ${data}`);
+// });
 
 socket.on('connect', () => {
 	console.log('Connected!');
