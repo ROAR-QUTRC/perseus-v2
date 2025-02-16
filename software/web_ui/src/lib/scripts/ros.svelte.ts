cedb0620e69dd6f709dfa451acf7c0510676491c
DingoOz <1388693+DingoOz@users.noreply.github.com>
2025-02-16 11:49:31 +0000

    chore: Format and lint

--- software/web_ui/src/lib/scripts/ros.svelte.ts
+++ software/web_ui/src/lib/scripts/ros.svelte.ts
@@ -1,8 +1,8 @@
-import ROSLIB from 'roslib';
-import { onDestroy } from 'svelte';
+import ROSLIB from "roslib";
+import { onDestroy } from "svelte";
 
 export let ros = $state<{ value: ROSLIB.Ros | null }>({
-	value: null
+  value: null,
 });
 
 let connected = $state<boolean>(false);
@@ -10,27 +10,27 @@ let retryTimeout: number = 0;
 
 export let ipAddress: string | null = null;
 export const isConnected = () => {
-	return connected;
+  return connected;
 };
 
 export const connect = (address: string) => {
-	ros.value = new ROSLIB.Ros({
-		url: `ws://${address}:9090`
-	});
+  ros.value = new ROSLIB.Ros({
+    url: `ws://${address}:9090`,
+  });
 
-	ros.value.on('connection', () => {
-		console.log('Connected to rosbridge server.');
-		connected = true;
-		ipAddress = address;
-	});
+  ros.value.on("connection", () => {
+    console.log("Connected to rosbridge server.");
+    connected = true;
+    ipAddress = address;
+  });
 
-	ros.value.on('error', (error) => {
-		console.log('Error connecting to websocket server: ', error);
-	});
+  ros.value.on("error", (error) => {
+    console.log("Error connecting to websocket server: ", error);
+  });
 
-	ros.value.on('close', () => {
-		console.log('Connection to websocket server closed.');
-		// TODO: retry after a time out that increases exponentially
-		connected = false;
-	});
+  ros.value.on("close", () => {
+    console.log("Connection to websocket server closed.");
+    // TODO: retry after a time out that increases exponentially
+    connected = false;
+  });
 };
--- software/web_ui/src/lib/widgets/videoWebrtc/webrtc.ts
+++ software/web_ui/src/lib/widgets/videoWebrtc/webrtc.ts
@@ -1,161 +1,169 @@
 export interface WebRtcSessionType {
-	name: string;
-	ip: string;
-	port: string;
-	signallingSocket?: WebSocket;
-	enableDebugging?: boolean;
+  name: string;
+  ip: string;
+  port: string;
+  signallingSocket?: WebSocket;
+  enableDebugging?: boolean;
 
-	clientId?: string | null;
-	producerId?: string | null;
-	sessionId?: string | null;
+  clientId?: string | null;
+  producerId?: string | null;
+  sessionId?: string | null;
 
-	peerConnection?: RTCPeerConnection;
-	tracks: MediaStream[];
+  peerConnection?: RTCPeerConnection;
+  tracks: MediaStream[];
 
-	// onNewTrack: (
-	// 	track: MediaStream,
-	// 	sessionInfo: { clientId: string; producerId: string; sessionId: string }
-	// ) => void;
+  // onNewTrack: (
+  // 	track: MediaStream,
+  // 	sessionInfo: { clientId: string; producerId: string; sessionId: string }
+  // ) => void;
 }
 
 export class WebRtcSession {
-	name: string;
-	ip: string;
-	port: string;
-	signallingSocket: WebSocket;
-	enableDebugging: boolean;
+  name: string;
+  ip: string;
+  port: string;
+  signallingSocket: WebSocket;
+  enableDebugging: boolean;
 
-	clientId: string | null = null;
-	producerId: string | null = null;
-	sessionId: string | null = null;
+  clientId: string | null = null;
+  producerId: string | null = null;
+  sessionId: string | null = null;
 
-	peerConnection: RTCPeerConnection;
+  peerConnection: RTCPeerConnection;
 
-	onNewTrack:
-		| ((
-				track: MediaStream,
-				sessionInfo: { clientId: string; producerId: string; sessionId: string }
-		  ) => void)
-		| null = null;
+  onNewTrack:
+    | ((
+        track: MediaStream,
+        sessionInfo: {
+          clientId: string;
+          producerId: string;
+          sessionId: string;
+        },
+      ) => void)
+    | null = null;
 
-	constructor(name: string, ip: string, port: string, debug: boolean = false) {
-		this.name = name;
-		this.ip = ip;
-		this.port = port;
-		this.enableDebugging = debug;
+  constructor(name: string, ip: string, port: string, debug: boolean = false) {
+    this.name = name;
+    this.ip = ip;
+    this.port = port;
+    this.enableDebugging = debug;
 
-		this.signallingSocket = new WebSocket(`ws://${ip}:${port}`);
+    this.signallingSocket = new WebSocket(`ws://${ip}:${port}`);
 
-		this.debug(`WebRtcSession: ${this.name} created`);
+    this.debug(`WebRtcSession: ${this.name} created`);
 
-		this.signallingSocket.onmessage = this.onSignal;
+    this.signallingSocket.onmessage = this.onSignal;
 
-		this.peerConnection = new RTCPeerConnection();
-		this.peerConnection.onicecandidate = (event) => {
-			this.debug('onicecandidate:', event);
-			if (event.candidate && this.sessionId !== null) {
-				this.sendSignal({
-					type: 'peer',
-					sessionId: this.sessionId,
-					ice: event.candidate.toJSON()
-				});
-			}
-		};
+    this.peerConnection = new RTCPeerConnection();
+    this.peerConnection.onicecandidate = (event) => {
+      this.debug("onicecandidate:", event);
+      if (event.candidate && this.sessionId !== null) {
+        this.sendSignal({
+          type: "peer",
+          sessionId: this.sessionId,
+          ice: event.candidate.toJSON(),
+        });
+      }
+    };
 
-		this.peerConnection.ontrack = (event) => {
-			this.debug('ontrack:', event);
-			this.debug(this.peerConnection);
-			if (this.onNewTrack)
-				this.onNewTrack(new MediaStream([event.track]), {
-					clientId: this.clientId!,
-					producerId: this.producerId!,
-					sessionId: this.sessionId!
-				});
-		};
-	}
+    this.peerConnection.ontrack = (event) => {
+      this.debug("ontrack:", event);
+      this.debug(this.peerConnection);
+      if (this.onNewTrack)
+        this.onNewTrack(new MediaStream([event.track]), {
+          clientId: this.clientId!,
+          producerId: this.producerId!,
+          sessionId: this.sessionId!,
+        });
+    };
+  }
 
-	private sendSignal = (message: any) => {
-		if (this.signallingSocket.readyState === WebSocket.OPEN) {
-			this.debug('sending:', message);
-			this.signallingSocket.send(JSON.stringify(message));
-		} else {
-			this.debug(`SOCKET ERROR: socket not open`);
-		}
-	};
+  private sendSignal = (message: any) => {
+    if (this.signallingSocket.readyState === WebSocket.OPEN) {
+      this.debug("sending:", message);
+      this.signallingSocket.send(JSON.stringify(message));
+    } else {
+      this.debug(`SOCKET ERROR: socket not open`);
+    }
+  };
 
-	private onSignal = (message: any) => {
-		const data = JSON.parse(message.data);
-		// this.debug('received:', data);
-		switch (data.type) {
-			case 'welcome':
-				this.clientId = data.peerId;
-				this.debug(`client id: ${this.clientId}`);
-				// register as a listener
-				this.sendSignal({ type: 'setPeerStatus', roles: ['listener'], meta: { name: this.name } });
-				break;
-			case 'peerStatusChanged':
-				if (data.roles.includes('listener')) {
-					this.debug('new listener:', data.peerId);
-					this.sendSignal({
-						type: 'list'
-					});
-				}
-				if (data.roles.includes('producer')) {
-					this.debug('new producer:', data.peerId);
-					this.producerId = data.peerId;
-					if (this.producerId) this.call();
-				}
-				break;
-			case 'list':
-				if (data.producers.length > 0) {
-					this.producerId = data.producers[0].id;
-					this.debug('Found produceers:', data.producers);
-					if (this.producerId) this.call();
-				}
-			case 'sessionStarted':
-				this.sessionId = data.sessionId;
-				break;
-			case 'peer':
-				if (data.sdp) {
-					this.peerConnection
-						.setRemoteDescription(data.sdp)
-						.then(() => {
-							return this.peerConnection.createAnswer();
-						})
-						.then((answer) => {
-							return this.peerConnection.setLocalDescription(answer);
-						})
-						.then(() => {
-							this.sendSignal({
-								type: 'peer',
-								sessionId: data.sessionId,
-								sdp: this.peerConnection.localDescription!.toJSON()
-							});
-						});
-				} else if (data.ice) {
-					this.peerConnection.addIceCandidate(new RTCIceCandidate(data.ice));
-				} else {
-					console.log('Unknown peer message:', data);
-				}
-				break;
-			default:
-				this.debug('received:', data);
-				break;
-		}
-	};
+  private onSignal = (message: any) => {
+    const data = JSON.parse(message.data);
+    // this.debug('received:', data);
+    switch (data.type) {
+      case "welcome":
+        this.clientId = data.peerId;
+        this.debug(`client id: ${this.clientId}`);
+        // register as a listener
+        this.sendSignal({
+          type: "setPeerStatus",
+          roles: ["listener"],
+          meta: { name: this.name },
+        });
+        break;
+      case "peerStatusChanged":
+        if (data.roles.includes("listener")) {
+          this.debug("new listener:", data.peerId);
+          this.sendSignal({
+            type: "list",
+          });
+        }
+        if (data.roles.includes("producer")) {
+          this.debug("new producer:", data.peerId);
+          this.producerId = data.peerId;
+          if (this.producerId) this.call();
+        }
+        break;
+      case "list":
+        if (data.producers.length > 0) {
+          this.producerId = data.producers[0].id;
+          this.debug("Found produceers:", data.producers);
+          if (this.producerId) this.call();
+        }
+      case "sessionStarted":
+        this.sessionId = data.sessionId;
+        break;
+      case "peer":
+        if (data.sdp) {
+          this.peerConnection
+            .setRemoteDescription(data.sdp)
+            .then(() => {
+              return this.peerConnection.createAnswer();
+            })
+            .then((answer) => {
+              return this.peerConnection.setLocalDescription(answer);
+            })
+            .then(() => {
+              this.sendSignal({
+                type: "peer",
+                sessionId: data.sessionId,
+                sdp: this.peerConnection.localDescription!.toJSON(),
+              });
+            });
+        } else if (data.ice) {
+          this.peerConnection.addIceCandidate(new RTCIceCandidate(data.ice));
+        } else {
+          console.log("Unknown peer message:", data);
+        }
+        break;
+      default:
+        this.debug("received:", data);
+        break;
+    }
+  };
 
-	private call() {
-		this.sendSignal({
-			type: 'startSession',
-			peerId: this.producerId
-		});
-	}
+  private call() {
+    this.sendSignal({
+      type: "startSession",
+      peerId: this.producerId,
+    });
+  }
 
-	private debug(...args: any[]) {
-		if (this.enableDebugging) {
-			for (let i = 0; i < args.length; i++) {
-				console.log(`[WebRtc - ${this.name}]`, args[i]);
-			}
-		}
-	}
+  private debug(...args: any[]) {
+    if (this.enableDebugging) {
+      for (let i = 0; i < args.length; i++) {
+        console.log(`[WebRtc - ${this.name}]`, args[i]);
+      }
+    }
+  }
 }
