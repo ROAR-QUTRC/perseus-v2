6694140bf09aa62b8a7055a8fff1b448d721a56b
DingoOz <dingo.australia@gmail.com>
2025-02-17 06:49:10 +1000

    perseus_arm_teleop: Corrected CMakeLists.txt

--- software/ros_ws/nix-packages/overlay.nix
+++ software/ros_ws/nix-packages/overlay.nix
@@ -1,10 +1,11 @@
-final: prev: {
-  autonomy = final.callPackage ./autonomy.nix { };
-  input-devices = final.callPackage ./input-devices.nix { };
-  perseus = final.callPackage ./perseus.nix { };
-  perseus-description = final.callPackage ./perseus-description.nix { };
-  perseus-hardware = final.callPackage ./perseus-hardware.nix { };
-  perseus-payloads = final.callPackage ./perseus-payloads.nix { };
-  perseus-sensors = final.callPackage ./perseus-sensors.nix { };
-  perseus-simulation = final.callPackage ./perseus-simulation.nix { };
+final: prev:
+{
+  autonomy = final.callPackage ./autonomy.nix {};
+  input-devices = final.callPackage ./input-devices.nix {};
+  perseus = final.callPackage ./perseus.nix {};
+  perseus-description = final.callPackage ./perseus-description.nix {};
+  perseus-hardware = final.callPackage ./perseus-hardware.nix {};
+  perseus-payloads = final.callPackage ./perseus-payloads.nix {};
+  perseus-sensors = final.callPackage ./perseus-sensors.nix {};
+  perseus-simulation = final.callPackage ./perseus-simulation.nix {};
 }
