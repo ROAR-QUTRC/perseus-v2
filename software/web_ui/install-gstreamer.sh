b4ca1906a90e32146e63ddd53115b718a2fe4e08
DingoOz <dingo.australia@gmail.com>
2025-02-16 21:32:58 +1000

    web-ui: added shebang to gstreamer install script

--- software/web_ui/install-gstreamer.sh
+++ software/web_ui/install-gstreamer.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail # Fail on errors, undefined vars, and pipe failures
+
 # Install gstreamer
 sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 
@@ -6,7 +9,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # build the plugin
 git clone https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs.git
-cd gst-plugins-rs/net/webrtc
+cd gst-plugins-rs/net/webrtc || exit
 sudo apt install libssl-dev
 cargo build
 mkdir -p ~/.local/share/gstreamer-1.0/plugins/
