6b7903564684a4dbc7a4c730c1db8dcd326e3446
James N <59348282+RandomSpaceship@users.noreply.github.com>
2025-02-19 17:24:44 +1000

    wip, squash later [no ci]

--- flake.nix
+++ flake.nix
@@ -131,6 +131,12 @@
             yarn
             nixgl-script
             ;
+          inherit (pkgs.gst_all_1)
+            gstreamer
+            gst-plugins-base
+            gst-plugins-good
+            gst-plugins-rs
+            ;
           inherit (pkgs.ros)
             rosbridge-suite
             rviz2-fixed
--- software/ros_ws/patches.nix
+++ software/ros_ws/patches.nix
@@ -40,4 +40,58 @@ in
     # so that they propagate properly
     ${rosDistro} = prev.rosPackages.${rosDistro}.overrideScope rosOverlay;
   };
+
+  gst_all_1 = prev.gst_all_1 // {
+    gst-plugins-rs =
+      (prev.gst_all_1.gst-plugins-rs.overrideAttrs (
+        {
+          ...
+        }:
+        rec {
+          version = "1.14.0-beta";
+          src = final.fetchFromGitLab {
+            domain = "gitlab.freedesktop.org";
+            owner = "gstreamer";
+            repo = "gst-plugins-rs";
+            rev = "9b0aa9c710874c07f37cf37f964e9942f9d89640";
+            # hash = "sha256-4qySdwd0Zo3HT7hm9+x9BjhuxO2Y4TBcDwWprnIJq54=";
+            hash = "sha256-WNuQEu77vr4+cYlY7vrhVRqIpgc6v1PiqLlhUHDZ4SI=";
+            # TODO: temporary workaround for case-insensitivity problems with color-name crate - https://github.com/annymosse/color-name/pull/2
+            postFetch = ''
+              sedSearch="$(cat <<\EOF | sed -ze 's/\n/\\n/g'
+              \[\[package\]\]
+              name = "color-name"
+              version = "\([^"\n]*\)"
+              source = "registry+https://github.com/rust-lang/crates.io-index"
+              checksum = "[^"\n]*"
+              EOF
+              )"
+              sedReplace="$(cat <<\EOF | sed -ze 's/\n/\\n/g'
+              [[package]]
+              name = "color-name"
+              version = "\1"
+              source = "git+https://github.com/lilyinstarlight/color-name#cac0ed5b7d2e0682c08c9bfd13089d5494e81b9a"
+              EOF
+              )"
+              sed -i -ze "s|$sedSearch|$sedReplace|g" $out/Cargo.lock
+            '';
+          };
+          cargoDeps =
+            with final;
+            rustPlatform.fetchCargoVendor {
+              inherit src;
+              name = final.gst_all_1.gst-plugins-rs.name;
+              hash = "sha256-HHMZ8kBA8g2cjYxrZnEsX33xC3glyKius3jxP84H+PY=";
+            };
+        }
+      )
+
+      ).override
+        ({
+          plugins = [
+            "rtp"
+            "webrtc"
+          ];
+        });
+  };
 }
