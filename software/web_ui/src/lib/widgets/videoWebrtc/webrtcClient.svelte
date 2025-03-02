49830ca9a020cb968cd43e077d7fa15d0591bcb6
OMEN44 <huonswales@gmail.com>
2025-03-02 13:34:38 +1000

    Removed unused files

--- software/web_ui/src/lib/widgets/videoWebrtc/webrtcClient.svelte
+++ software/web_ui/src/lib/widgets/videoWebrtc/webrtcClient.svelte
@@ -142,7 +142,7 @@
 	{#each tracks as track, i}
 		<div class="relative">
 			<VideoWrapper media={track} />
-			<p class="bg-card absolute bottom-1 left-1 rounded-[4px] bg-opacity-60 px-2 py-1">
+			<p class="absolute bottom-1 left-1 rounded-[4px] bg-card bg-opacity-60 px-2 py-1">
 				{cameras[i].name}
 			</p>
 		</div>
