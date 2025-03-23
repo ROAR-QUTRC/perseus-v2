ea1880b380124e4d1e932a92cb70e91663cd1aa5
ROAR Rover <182080788+ROAR-Rover@users.noreply.github.com>
2025-03-23 15:12:23 +1000

    Got Livox working on ROS2

--- software/ros_ws/src/perseus_sensors/launch/test_livox.launch.py
+++ software/ros_ws/src/perseus_sensors/launch/test_livox.launch.py
@@ -1,32 +1,34 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import LogInfo
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
+
 def generate_launch_description():
     # Construct the path to the config file
-    config_path = PathJoinSubstitution([
-        FindPackageShare('perseus_sensors'),
-        'config',
-        'livox_config.json'
-    ])
+    config_path = PathJoinSubstitution(
+        [FindPackageShare("perseus_sensors"), "config", "livox_config.json"]
+    )
 
-    return LaunchDescription([
-        Node(
-            package="livox_ros_driver2",
-            executable="livox_ros_driver2_node",
-            name="livox_lidar_publisher",
-            parameters=[
-                {
-                    "xfer_format": 0,
-                    "multi_topic": 0,
-                    "data_src": 0,
-                    "publish_freq": 10.0,
-                    "output_data_type": 0,
-                    "frame_id": "livox_frame",
-                    "user_config_path": config_path,  # Use the resolved path here
-                }
-            ],
-            output="screen",
-        ),
-    ])
+    return LaunchDescription(
+        [
+            Node(
+                package="livox_ros_driver2",
+                executable="livox_ros_driver2_node",
+                name="livox_lidar_publisher",
+                parameters=[
+                    {
+                        "xfer_format": 0,
+                        "multi_topic": 0,
+                        "data_src": 0,
+                        "publish_freq": 10.0,
+                        "output_data_type": 0,
+                        "frame_id": "livox_frame",
+                        "user_config_path": config_path,  # Use the resolved path here
+                    }
+                ],
+                output="screen",
+            ),
+        ]
+    )
