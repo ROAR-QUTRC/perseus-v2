15540addbb2b187fa9cb38183ea0f9164054bac1
OMEN44 <huonswales@gmail.com>
2025-03-24 13:19:32 +1000

    Light tower code

--- firmware/light-tower/src/main.cpp
+++ firmware/light-tower/src/main.cpp
@@ -1,26 +1,61 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-#define DATA_PIN 12
-#define NUM_LEDS 60
+#define DATA_PIN 15
+#define NUM_LEDS 90 + 60 * 4
 CRGB leds[NUM_LEDS];
 
 void setup()
 {
-    FastLED.addLeds<WS2811, DATA_PIN, GRB>(leds, NUM_LEDS);  // BRG is the order of this specific strip, usually they are RGB.
+    pinMode(15, OUTPUT);
+    FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);  // BRG is the order of this specific strip, usually they are RGB.
+}
+
+void light(unsigned band, CRGB color, int brightness = 255)
+{
+    int ledBandSize = 90;  // number of leds in a band
+    int ledBuffer = 0;     // the number of leds to skip
+
+    if (band > 0)
+    {
+        ledBandSize = 60;
+        ledBuffer = 90 + (band - 1) * 60;
+    }
+
+    for (int i = 0; i < ledBandSize; i++)
+    {
+        leds[i + ledBuffer] = color;
+    }
+    FastLED.setBrightness(brightness);
+    FastLED.show();
 }
 
 void loop()
 {
-    // Turn the LED on, then pause
-    for (int i = 0; i <= 32; i++)
-    {  // each band contains 8 segments of LEDs, 0 is first, 44 is last (top).
-        leds[i] = CRGB(0, 255, 0);
-    }
-    for (int i = 32; i <= 44; i++)
-    {
-        leds[i] = CRGB(255, 255, 255);
-    }
-    FastLED.show();
-    delay(500);
+    light(0, CRGB::Red, 128);
+    delay(1000);
+    light(0, CRGB::Red);
+    delay(1000);
+    light(0, CRGB::Orange, 128);
+    delay(1000);
+    light(0, CRGB::Orange);
+    delay(1000);
+    light(0, CRGB::Yellow, 128);
+    delay(1000);
+    light(0, CRGB::Yellow);
+    delay(1000);
+    light(0, CRGB::Green, 128);
+    delay(1000);
+    light(0, CRGB::Green);
+    delay(1000);
+    light(0, CRGB::Blue);
+    delay(1000);
+    light(0, CRGB::Indigo);
+    delay(1000);
+    light(0, CRGB::Violet);
+    delay(1000);
+    light(0, CRGB::White);
+    delay(1000);
+    light(0, CRGB::Black);
+    delay(1000);
 }
