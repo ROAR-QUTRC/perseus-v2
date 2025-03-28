7aea6b15135c6d3c3c52ff2388fa89e5017c4660
James N <59348282+RandomSpaceship@users.noreply.github.com>
2025-03-29 08:58:45 +1000

    wip: rtabmap building?

--- software/ros_ws/third-party-packages/overlay.nix
+++ software/ros_ws/third-party-packages/overlay.nix
@@ -3,6 +3,7 @@ let
   individualPackages = prev: final: {
     ortools = final.callPackage ./ortools { };
     livox-ros-driver2 = final.callPackage ./livox-ros-driver2 { };
+    fast-lio = final.callPackage ./fast-lio { };
   };
 in
 prev.lib.composeManyExtensions [
