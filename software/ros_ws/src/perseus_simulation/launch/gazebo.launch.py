ce9bcb653363f0ce17b78bcff3c1efabcca9122a
Kelvin Le <nguyenleminh1002@gmail.com>
2025-08-22 15:34:08 +1000

    Add Perseus Arc World and Moon DEM models for simulation
    - Updated CMakeLists.txt to include models directory for installation.
    - Modified gazebo.launch.py to set the default world to perseus_arc_world.world and configured model paths.
    - Added Moon DEM model files including textures, heightmap, and SDF configuration.
    - Created Perseus Arc World model with various obstacles and walls, including ArUco markers.
    - Added textures and materials for obstacles and walls.
    - Implemented world file for the Perseus Arc simulation environment.

--- software/ros_ws/src/perseus_simulation/launch/gazebo.launch.py
+++ software/ros_ws/src/perseus_simulation/launch/gazebo.launch.py
@@ -4,14 +4,16 @@ from launch.substitutions import (
     PathJoinSubstitution,
     LaunchConfiguration,
 )
-
+import os
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
     # ARGUMENTS
-    gz_world = LaunchConfiguration("gz_world", default="perseus_world.sdf")
+    gz_world = LaunchConfiguration("gz_world", default="perseus_arc_world.world")
 
     # CONFIG + DATA FILES
     gz_bridge_params = PathJoinSubstitution(
@@ -38,6 +40,10 @@ def generate_launch_description():
         # As such, we need to just run it directly - but we also need to resolve the world substitution,
         # hence wrapping in an OpaqueFunction.
         performed_gz_world_path = gz_world_path.perform(context)
+
+        model_path = os.path.join(
+            get_package_share_directory("perseus_simulation"), "models"
+        )
         gz_launch = ExecuteProcess(
             cmd=[
                 "nix",
@@ -56,6 +62,8 @@ def generate_launch_description():
                 "NIXPKGS_ALLOW_UNFREE": "1",
                 "QT_QPA_PLATFORM": "xcb",
                 "QT_SCREEN_SCALE_FACTORS": "1",
+                "PROJ_IGNORE_CELESTIAL_BODY": "YES",  # Fixed here
+                "GZ_SIM_RESOURCE_PATH": model_path,  # Ensure the model path is set correctly
             },
         )
 
@@ -77,6 +85,7 @@ def generate_launch_description():
     # )
     launch_files = [
         # gz_launch,
+        # set_env_vars_resources,
         OpaqueFunction(function=gz_launch),
     ]
 
