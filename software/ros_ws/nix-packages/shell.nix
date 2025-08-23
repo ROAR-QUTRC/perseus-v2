3143dcca01ea3f7bfd6ded82e5e73ee35cdd4cb7
Kelvin Le <nguyenleminh1002@gmail.com>
2025-08-23 10:27:14 +1000

    feat: Update Nix package configurations and add vision package for Perseus robot

--- software/ros_ws/nix-packages/shell.nix
+++ software/ros_ws/nix-packages/shell.nix
@@ -37,17 +37,20 @@ pkgs.mkShell {
             ament-pep257
             backward-ros
             controller-manager
+            cv-bridge
             diff-drive-controller
             geometry-msgs
             gz-ros2-control
             hardware-interface
             hi-can
             hi-can-raw
+            image-transport
             joint-state-broadcaster
             joy
             mecanum-drive-controller
             navigation2
             nlohmann_json
+            opencv
             openssl
             pluginlib
             python3Packages.pygame
@@ -71,6 +74,8 @@ pkgs.mkShell {
             slam-toolbox
             teleop-twist-joy
             teleop-twist-keyboard
+            tf2
+            tf2-ros
             twist-mux
             xacro
           ]
@@ -78,15 +83,14 @@ pkgs.mkShell {
         ++ builtins.attrValues extraPkgs
         ++ extraPaths
         ++ withPackages (pkgs // pkgs.rosPackages.${rosDistro});
-    })
+      }
+    )
   ];
-  shellHook =
-    ''
-      # Setup ROS 2 shell completion. Doing it in direnv is useless.
-      if [[ ! $DIRENV_IN_ENVRC ]]; then
-          eval "$(${pkgs.python3Packages.argcomplete}/bin/register-python-argcomplete ros2)"
-          eval "$(${pkgs.python3Packages.argcomplete}/bin/register-python-argcomplete colcon)"
-      fi
-    ''
-    + extraShellHook;
+  shellHook = ''
+    # Setup ROS 2 shell completion. Doing it in direnv is useless.
+    if [[ ! $DIRENV_IN_ENVRC ]]; then
+        eval "$(${pkgs.python3Packages.argcomplete}/bin/register-python-argcomplete ros2)"
+        eval "$(${pkgs.python3Packages.argcomplete}/bin/register-python-argcomplete colcon)"
+    fi
+  '' + extraShellHook;
 }
