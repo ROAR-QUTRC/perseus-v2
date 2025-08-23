dde72c1e2d0a3dfd2ff1d702a70b2da7891f313e
Kelvin Le <nguyenleminh1002@gmail.com>
2025-08-23 10:27:14 +1000

    feat: Update Nix package configurations and add vision package for Perseus robot

--- software/ros_ws/nix-packages/overlay.nix
+++ software/ros_ws/nix-packages/overlay.nix
@@ -1,15 +1,16 @@
-final: prev: {
-  autonomy = final.callPackage ./autonomy.nix { };
-  input-devices = final.callPackage ./input-devices.nix { };
-  perseus = final.callPackage ./perseus.nix { };
-  perseus-can-if = final.callPackage ./perseus-can-if.nix { };
-  perseus-description = final.callPackage ./perseus-description.nix { };
-  perseus-hardware = final.callPackage ./perseus-hardware.nix { };
-  perseus-input = final.callPackage ./perseus-input.nix { };
-  perseus-input-config = final.callPackage ./perseus-input-config.nix { };
-  perseus-payloads = final.callPackage ./perseus-payloads.nix { };
-  perseus-sensors = final.callPackage ./perseus-sensors.nix { };
-  perseus-simulation = final.callPackage ./perseus-simulation.nix { };
-  perseus-teleop = final.callPackage ./perseus-teleop.nix { };
-  perseus-vision = final.callPackage ./perseus-vision.nix { };
+final: prev:
+{
+  autonomy = final.callPackage ./autonomy.nix {};
+  input-devices = final.callPackage ./input-devices.nix {};
+  perseus = final.callPackage ./perseus.nix {};
+  perseus-can-if = final.callPackage ./perseus-can-if.nix {};
+  perseus-description = final.callPackage ./perseus-description.nix {};
+  perseus-hardware = final.callPackage ./perseus-hardware.nix {};
+  perseus-input = final.callPackage ./perseus-input.nix {};
+  perseus-input-config = final.callPackage ./perseus-input-config.nix {};
+  perseus-payloads = final.callPackage ./perseus-payloads.nix {};
+  perseus-sensors = final.callPackage ./perseus-sensors.nix {};
+  perseus-simulation = final.callPackage ./perseus-simulation.nix {};
+  perseus-teleop = final.callPackage ./perseus-teleop.nix {};
+  perseus-vision = final.callPackage ./perseus-vision.nix {};
 }
