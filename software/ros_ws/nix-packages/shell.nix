e2d4a246168ffb1c63a3b68bd200312506a812db
Kelvin Le <nguyenleminh1002@gmail.com>
2025-09-13 00:51:22 +1000

    feat: update Nix package configurations and add ONNX Runtime support

--- software/ros_ws/nix-packages/shell.nix
+++ software/ros_ws/nix-packages/shell.nix
@@ -51,6 +51,7 @@ pkgs.mkShell {
             mecanum-drive-controller
             navigation2
             nlohmann_json
+            onnxruntime
             opencv
             openssl
             pluginlib
@@ -84,15 +85,14 @@ pkgs.mkShell {
         ++ builtins.attrValues extraPkgs
         ++ extraPaths
         ++ withPackages (pkgs // pkgs.rosPackages.${rosDistro});
-    })
+      }
+    )
   ];
-  shellHook =
-    ''
-      # Setup ROS 2 shell completion. Doing it in direnv is useless.
-      if [[ ! $DIRENV_IN_ENVRC ]]; then
-          eval "$(${pkgs.python3Packages.argcomplete}/bin/register-python-argcomplete ros2)"
-          eval "$(${pkgs.python3Packages.argcomplete}/bin/register-python-argcomplete colcon)"
-      fi
-    ''
-    + extraShellHook;
+  shellHook = ''
+    # Setup ROS 2 shell completion. Doing it in direnv is useless.
+    if [[ ! $DIRENV_IN_ENVRC ]]; then
+        eval "$(${pkgs.python3Packages.argcomplete}/bin/register-python-argcomplete ros2)"
+        eval "$(${pkgs.python3Packages.argcomplete}/bin/register-python-argcomplete colcon)"
+    fi
+  '' + extraShellHook;
 }
