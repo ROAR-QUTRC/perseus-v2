8655f448d1ca16cb3f5e9ab4448b1e603ae14e60
OMEN44 <huonswales@gmail.com>
2025-11-26 17:41:04 +1000

    Fixed rcb driver node

--- software/web_ui/src/lib/widgets/busPowerManager.svelte
+++ software/web_ui/src/lib/widgets/busPowerManager.svelte
@@ -19,9 +19,23 @@
 	import { faPowerOff } from '@fortawesome/free-solid-svg-icons';
 	import { onMount } from 'svelte';
 	import { sentenceCase } from 'change-case';
+	import * as Dialog from '$lib/components/ui/dialog/index';
+	import { Button } from '$lib/components/ui/button';
 
-	// Widget logic goes here
-	let busState = $state<Record<string, { status: string; current: string; voltage: string }>>({});
+	const busStatus = [
+		'OFF',
+		'ON',
+		'PRECHARGING',
+		'SHORT_CIRCUIT',
+		'SWITCH_FAILED',
+		'OVERLOAD',
+		'FAULT'
+	];
+
+	let listener: ROSLIB.Topic;
+	let busState = $state<Record<string, { status: number; current: string; voltage: string }>>({});
+
+	$inspect(busState);
 
 	const toggleBusPower = (e: Event, bus: string) => {
 		e.preventDefault();
@@ -32,16 +46,14 @@
 		});
 
 		const message = new ROSLIB.Message({
-			data: JSON.stringify({ bus: bus, on: busState[bus].status ? '1' : '0' })
+			data: JSON.stringify({ bus: bus, on: busState[bus].status !== 1 ? '1' : '0' })
 		});
 
 		publisher.publish(message);
 		publisher.unsubscribe();
 	};
 
-	onMount(() => {
-		let listener: ROSLIB.Topic;
-
+	$effect(() => {
 		if (getRosConnection()) {
 			listener = new ROSLIB.Topic({
 				ros: getRosConnection() as ROSLIB.Ros,
@@ -50,30 +62,56 @@
 			});
 
 			listener.subscribe((message: any) => {
-				busState = JSON.parse(message.data);
+				const { name, status, ...busData } = JSON.parse(message.data);
+				busState[name] = { status: Number(status), ...busData };
 			});
 		}
+	});
 
+	onMount(() => {
 		return () => {
 			listener.unsubscribe();
 		};
 	});
+
+	// Confirm menu
+	let openConfirm: Record<string, boolean> = {};
+	// $effect(() => {
+	// 	for (const bus of Object.keys(busState)) {
+	// 		if (!(bus in openConfirm)) {
+	// 			openConfirm[bus] = false;
+	// 		}
+	// 	}
+	// });
 </script>
 
-<div class="flex h-full w-full">
+<div class="flex w-full">
 	{#each Object.keys(busState) as bus, i}
-		<div class="mx-[5px] mb-auto w-[calc(25%-10px)] min-w-[60px] max-w-[130px] flex-col">
+		<div
+			class="mx-[5px] mb-auto flex w-[calc(25%-10px)] min-w-[60px] max-w-[130px] flex-col justify-center rounded-lg border p-2"
+		>
 			<p class="mb-2 text-center">{sentenceCase(bus)}</p>
-			<button
-				class="aspect-square cursor-pointer rounded-[50%] border"
-				onclick={(e) => toggleBusPower(e, bus)}
-			>
-				<Fa
-					icon={faPowerOff}
-					class="power-button"
-					color={busState[bus].status == '1' ? '#f00' : '#0f0'}
-				/>
-			</button>
+
+			<Dialog.Root bind:open={openConfirm[bus]}>
+				<Dialog.Trigger>
+					<button
+						class="aspect-square cursor-pointer rounded-[50%] border"
+						onclick={() => (openConfirm[bus] = true)}
+					>
+						<Fa
+							icon={faPowerOff}
+							class="power-button w-full"
+							color={busState[bus].status == 1 ? '#0f0' : '#f00'}
+						/>
+					</button>
+				</Dialog.Trigger>
+				<Dialog.Content>
+					<Button>Cancel</Button>
+					<Button>Confirm</Button>
+				</Dialog.Content>
+			</Dialog.Root>
+
+			<p>{busStatus[busState[bus].status]}</p>
 			<p class="mt-2">Current: {busState[bus].current}</p>
 			<p>Voltage: {busState[bus].voltage}</p>
 		</div>
@@ -81,6 +119,7 @@
 		<p>Looking for buses... Make sure the rosbridge is connected.</p>
 	{/each}
 </div>
+<p>Note: <kbd>FAULT</kbd> on the drive bus likely indicates the drive stop is engaged.</p>
 
 <style>
 	:global(.power-button) {
