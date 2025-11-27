ad2fcee1fef41c4a2498b78269dd6818eafc10f6
OMEN44 <huonswales@gmail.com>
2025-11-27 16:37:48 +1000

    Finished bucket driver widget

--- software/web_ui/src/lib/widgets/bucketDrive.svelte
+++ software/web_ui/src/lib/widgets/bucketDrive.svelte
@@ -15,6 +15,26 @@
 					type: 'number',
 					description: 'Base multiplier applied to all speeds',
 					value: '1'
+				},
+				lift: {
+					type: 'number',
+					description: 'Multiplier for lift speed',
+					value: '0.1'
+				},
+				tilt: {
+					type: 'number',
+					description: 'Multiplier for tilt speed',
+					value: '0.1'
+				},
+				jaws: {
+					type: 'number',
+					description: 'Multiplier for jaws speed',
+					value: '0.1'
+				},
+				rotate: {
+					type: 'number',
+					description: 'Multiplier for rotate speed',
+					value: '0.5'
 				}
 			}
 		}
@@ -25,16 +45,15 @@
 	import { getRosConnection } from '$lib/scripts/ros-bridge.svelte'; // ROSLIBJS docs here: https://robotwebtools.github.io/roslibjs/Service.html
 	import ROSLIB from 'roslib';
 	import { onMount } from 'svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
 
-	// Widget logic goes here
-	$effect(() => {
-		const ros = getRosConnection();
-		if (!ros) return;
-	});
+	const CALLBACK_INTERVAL = 100; // 10Hz
+	const INACTIVE_TIMEOUT = 10000; // 10 seconds
 
-	// ---------------------------------------
-	// 		  Joystick HTML components
-	// ---------------------------------------
+	let intervalHandle: NodeJS.Timeout | null = null;
+	let lockingTimeoutHandle: NodeJS.Timeout | null = null;
+	let topic: ROSLIB.Topic | null = null;
+	let magnet = $state<boolean>(false);
 	const handles = $state<
 		Record<string, { parent: HTMLDivElement | null; active: boolean; value: number }>
 	>({
@@ -44,11 +63,108 @@
 		rotate: { parent: null, active: false, value: 0 }
 	});
 
+	// ROS2 connection management
+	$effect(() => {
+		const ros = getRosConnection();
+
+		if (ros) {
+			topic = new ROSLIB.Topic({
+				ros: ros,
+				name: '/bucket_actuators',
+				messageType: 'actuator_msgs/Actuators'
+			});
+			topic.advertise();
+		} else {
+			topic = null;
+		}
+	});
+
+	const deadBand = (value: number, threshold: number) => {
+		return Math.abs(value) < threshold ? 0 : value;
+	};
+
+	const publishActuatorValues = () => {
+		if (!topic) return;
+
+		const velocities = [
+			deadBand(handles.lift.value, 0.05) *
+				Number(settings.groups.general.lift.value) *
+				Number(settings.groups.general.masterSpeedMultiplier.value),
+			deadBand(handles.tilt.value, 0.05) *
+				Number(settings.groups.general.tilt.value) *
+				Number(settings.groups.general.masterSpeedMultiplier.value),
+			deadBand(handles.jaws.value, 0.05) *
+				Number(settings.groups.general.jaws.value) *
+				Number(settings.groups.general.masterSpeedMultiplier.value),
+			deadBand(handles.rotate.value, 0.05) *
+				Number(settings.groups.general.rotate.value) *
+				Number(settings.groups.general.masterSpeedMultiplier.value)
+		];
+
+		const message = new ROSLIB.Message({
+			header: {}, // Leaving this empty forces ROS bridge to fill in the timestamp.
+			velocity: velocities,
+			normalized: [magnet ? 1 : 0]
+		});
+
+		topic.publish(message);
+	};
+
+	// Unlock logic
+	let unlocked = $state(false);
+	let unlocking = false;
+
+	const startLockingTimer = () => {
+		return setTimeout(() => {
+			Object.keys(handles).forEach((handle) => {
+				onStop(handle);
+			});
+			publishActuatorValues();
+			if (intervalHandle) clearInterval(intervalHandle);
+			unlocked = false;
+		}, INACTIVE_TIMEOUT);
+	};
+
+	const startUnlockTimer = () => {
+		unlocking = true;
+		setTimeout(() => {
+			if (unlocking) {
+				unlocked = true;
+				unlocking = false;
+				initSliders();
+
+				// lock the sliders
+				lockingTimeoutHandle = startLockingTimer();
+			}
+		}, 1000);
+	};
+
+	const stopUnlockTimer = () => {
+		unlocking = false;
+		unlocked = false;
+	};
+
+	// Joystick HTML components
+	const move = (handle: string, clientY: number) => {
+		const boundingRect = handles[handle].parent?.getBoundingClientRect();
+		if (!boundingRect) return;
+		const clampedY = Math.min(Math.max(clientY, boundingRect.top), boundingRect.bottom);
+		handles[handle].value = -(((clampedY - boundingRect.top) / boundingRect.height) * 2 - 1);
+	};
+
 	const onStart = (event: PointerEvent, handle: string) => {
 		event.preventDefault();
 
 		handles[handle].active = true;
+		move(handle, event.clientY);
 		console.log('Start', handle, event.clientY);
+
+		// reset locking timeout
+		console.log('Resetting locking timeout', lockingTimeoutHandle);
+		if (lockingTimeoutHandle) {
+			clearTimeout(lockingTimeoutHandle);
+		}
+		lockingTimeoutHandle = null;
 	};
 
 	const onMove = (event: PointerEvent, handle: string) => {
@@ -56,85 +172,88 @@
 
 		event.preventDefault();
 
-		const boundingRect = handles[handle].parent?.getBoundingClientRect();
-		if (!boundingRect) return;
-
-		// Thumb padding 5px + 25px radius
-		const padding = 5 + 25;
-		const clampedY = Math.round(
-			Math.max(boundingRect.top + padding, Math.min(boundingRect.bottom - padding, event.clientY))
-		);
-		handles[handle].value = (clampedY - boundingRect.top) / boundingRect.height;
-
-		console.log('Move', handle, event.clientY, boundingRect.top, boundingRect.bottom);
+		move(handle, event.clientY);
 	};
 
-	const onStop = (event: PointerEvent, handle: string) => {
-		if (!handles[handle].active) return;
-
-		event.preventDefault();
+	const onStop = (handle: string, event?: PointerEvent) => {
+		event?.preventDefault();
 
 		handles[handle].value = 0;
 		handles[handle].active = false;
-		console.log('Stop', handle, event.clientY);
+		console.log('Stop', handle, event?.clientY);
+
+		// start locking timeout
+		const allInactive = Object.keys(handles).every((h) => !handles[h].active);
+		if (allInactive && !lockingTimeoutHandle) {
+			lockingTimeoutHandle = startLockingTimer();
+		}
+	};
+
+	const initSliders = () => {
+		setTimeout(() => {
+			Object.keys(handles).forEach((handle) => {
+				if (handles[handle].parent) {
+					handles[handle].parent?.addEventListener('pointerdown', (e) => onStart(e, handle));
+					handles[handle].parent?.addEventListener('pointermove', (e) => onMove(e, handle));
+					handles[handle].parent?.addEventListener('pointerup', (e) => onStop(handle, e));
+					handles[handle].parent?.addEventListener('pointerleave', (e) => onStop(handle, e));
+					const rect = handles[handle].parent.getBoundingClientRect();
+					console.log('Initialised: ', rect);
+				} else console.warn('Failed to initialise: ', handle);
+			});
+		});
+
+		intervalHandle = setInterval(() => {
+			publishActuatorValues();
+		}, CALLBACK_INTERVAL);
 	};
 
 	onMount(() => {
-		Object.keys(handles).forEach((handle) => {
-			if (handles[handle].parent) {
-				handles[handle].parent?.addEventListener('pointerdown', (e) => onStart(e, handle));
-				handles[handle].parent?.addEventListener('pointermove', (e) => onMove(e, handle));
-				handles[handle].parent?.addEventListener('pointerup', (e) => onStop(e, handle));
-				handles[handle].parent?.addEventListener('pointerleave', (e) => onStop(e, handle));
-				const rect = handles[handle].parent.getBoundingClientRect();
-				console.log('Initialised: ', rect);
-			} else console.warn('Failed to initialise: ', handle);
-
-			// handles[handle].element.onpointerdown = (event) => {
-			// 	event.preventDefault();
-			// 	let startY = event.clientY;
-			// 	const onPointerMove = (moveEvent: PointerEvent) => {
-			// 		moveEvent.preventDefault();
-			// 		const deltaY = moveEvent.clientY - startY;
-			// 		startY = moveEvent.clientY;
-			// 		handles[handle].value -= deltaY * 0.1; // Adjust sensitivity as needed
-			// 		handles[handle].value = Math.max(-100, Math.min(100, handles[handle].value)); // Clamp between -100 and 100
-			// 	};
-			// 	const onPointerUp = (upEvent: PointerEvent) => {
-			// 		upEvent.preventDefault();
-			// 		window.removeEventListener('pointermove', onPointerMove);
-			// 		window.removeEventListener('pointerup', onPointerUp);
-			// 	};
-			// 	window.addEventListener('pointermove', onPointerMove);
-			// 	window.addEventListener('pointerup', onPointerUp);
-			// };
-		});
+		unlocked = false;
 
 		// Cleanup ros connection on unmount
-		return () => {};
+		return () => {
+			if (intervalHandle) clearInterval(intervalHandle);
+		};
 	});
 </script>
 
-<!-- Value is the height from -1 to 1 -->
-{#snippet thumb(hidden: boolean, value: number)}
-	{#if !hidden}
-		<div
-			style:top={`calc(${50 - value * 50}% - 25px)`}
-			class={'absolute left-[5px] z-30 h-[50px] w-[50px] rounded-[25px] bg-blue-500 opacity-50'}
-		></div>
-	{/if}
-{/snippet}
-
-<div class="flex h-full w-full gap-2 border">
-	{#each Object.keys(handles) as key}
-		<div class="flex flex-1 flex-col items-center justify-center gap-2 p-2">
-			<p>{key} {handles[key].value}</p>
+{#if !unlocked}
+	<div
+		class="absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] overflow-hidden text-xl"
+		style:z-index="3"
+	>
+		<Button variant="outline" onpointerdown={startUnlockTimer} onpointerup={stopUnlockTimer}>
+			Hold to unlock
+		</Button>
+	</div>
+{:else}
+	<!-- Value is the height from -1 to 1 -->
+	{#snippet thumb(active: boolean, value: number)}
+		{#if active}
 			<div
-				bind:this={handles[key].parent}
-				class=" relative z-20 h-[60%] w-[60px] rounded-[30px] bg-red-500 opacity-50"
-			>
-				{@render thumb(false, handles[key].value)}
-			</div>
+				style:top={`calc(${50 - value * 50}% - 25px)`}
+				class={'absolute left-[5px] z-50 h-[50px] w-[50px] rounded-[25px] bg-blue-500'}
+			></div>
+		{/if}
+	{/snippet}
+
+	<div class=" flex h-full w-full flex-col">
+		<div class="flex flex-1 gap-2">
+			{#each Object.keys(handles) as key}
+				<div class="flex flex-1 flex-col items-center justify-center gap-2 p-2">
+					<p class="text-center" style="text-transform: capitalize;">{key}</p>
+					<div
+						bind:this={handles[key].parent}
+						class=" relative z-40 h-[60%] w-[60px] rounded-[30px] bg-red-500 opacity-50"
+					>
+						{@render thumb(handles[key].active, handles[key].value)}
+					</div>
+				</div>
+			{/each}
 		</div>
-	{/each}
-</div>
+		<Button class="w-fit self-center" onclick={() => (magnet = !magnet)}
+			>Turn magnet {magnet ? 'off' : 'on'}</Button
+		>
+	</div>
+{/if}
