83df53077d724f48407b6de72d29227a9156f7cf
UltraFishy <103304914+UltraFishy@users.noreply.github.com>
2025-12-23 05:18:11 +0000

    chore: Format and lint

--- software/zellij-utils/start-bringup.sh
+++ software/zellij-utils/start-bringup.sh
@@ -5,8 +5,8 @@ LAYOUT=~/perseus-v2/software/zellij-utils/bringup-layout.kdl
 
 # If the session doesn't exist, start it and exit into it
 if ! zellij list-sessions | awk '{print $1}' | grep -qx "$SESSION"; then
-    echo "No existing session. Creating '$SESSION' with layout..."
-    exec zellij --layout "$LAYOUT" --session "$SESSION"
+  echo "No existing session. Creating '$SESSION' with layout..."
+  exec zellij --layout "$LAYOUT" --session "$SESSION"
 fi
 
 # Otherwise attach to it
