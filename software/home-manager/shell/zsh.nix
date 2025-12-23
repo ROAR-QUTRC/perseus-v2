a8aae0dfa0b5470643b2d1c5302daa9c67f65cf4
UltraFishy <UltraFishy@users.noreply.github.com>
2025-12-23 15:16:17 +1000

    Changing jetson config
    - Changed p10k to starship
    - Started a zellij layout for running Perseus

--- software/home-manager/shell/zsh.nix
+++ software/home-manager/shell/zsh.nix
@@ -27,27 +27,9 @@
       '';
     };
 
-    initExtraFirst = ''
-      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-      # Initialization code that may require console input (password prompts, [y/n]
-      # confirmations, etc.) must go above this block; everything else may go below.
-      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-      fi
+    initExtra = ''
+      eval "$(starship init zsh)"
     '';
-    plugins = [
-      # configure p10k
-      {
-        name = "p10k-config";
-        src = ./p10k;
-        file = "icons-concise.zsh";
-      }
-      # enable p10k
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-    ];
+
   };
 }
---
+++ software/zellij-utils/start-bringup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SESSION="bringup"
+LAYOUT=~/perseus-v2/software/zellij-utils/bringup-layout.kdl
+
+# If the session doesn't exist, start it and exit into it
+if ! zellij list-sessions | awk '{print $1}' | grep -qx "$SESSION"; then
+    echo "No existing session. Creating '$SESSION' with layout..."
+    exec zellij --layout "$LAYOUT" --session "$SESSION"
+fi
+
+# Otherwise attach to it
+echo "Attaching to existing session '$SESSION'"
+exec zellij attach "$SESSION"
