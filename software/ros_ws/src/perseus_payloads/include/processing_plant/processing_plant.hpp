a77d8b7713f225e4a91bfef982330f349b7ba42a
BrockMorgan88 <brockjamesmorgan@gmail.com>
2025-12-31 17:44:42 +1000

    feat: Added initialiser functions for i2c sensors

--- software/ros_ws/src/perseus_payloads/include/processing_plant/processing_plant.hpp
+++ software/ros_ws/src/perseus_payloads/include/processing_plant/processing_plant.hpp
@@ -1,8 +1,27 @@
 #pragma once
+#include <fstream>
 #include <rclcpp/rclcpp.hpp>
-
+#include <string>
 class ProcessingPlant : public rclcpp::Node
 {
 public:
     explicit ProcessingPlant(const rclcpp::NodeOptions& options = rclcpp::NodeOptions());
+    ~ProcessingPlant();
+
+private:
+    void _init_i2c();
+    int _check_sensor(const uint16_t address);
+    void _init_spectral();
+    void _init_magnetometer();
+    void _write_i2c(const uint16_t address, std::vector<uint8_t>* data);
+    std::vector<uint8_t> _read_write_i2c(const uint16_t address, std::vector<uint8_t>* send_data, uint16_t read_bytes);
+    const uint8_t _spectral_address = 0x00;  // TODO: Change this to the proper address
+    const uint8_t _spectral_enable_register = 0x80;
+    const uint8_t _spectral_enable_data = 0b00001011;
+    const uint8_t _spectral_data_N_L_register = 0x95;
+    const uint8_t _magnetometer_address = 0x01;  // TODO: Change this to the proper address
+    const uint8_t _magnetometer_enable_data = 0b11110000;
+    const uint8_t _magnetometer_enable_xyz = 0b00000111;
+    const std::string _i2c_filename = "/dev/i2c-0";  // TODO: Make this a ROS2 input option
+    int _i2c_file = -1;
 };
--- software/ros_ws/src/perseus_payloads/src/processing_plant/processing_plant.cpp
+++ software/ros_ws/src/perseus_payloads/src/processing_plant/processing_plant.cpp
@@ -1,9 +1,122 @@
+// Run this node on a raspberry pi or a device that has an i2c bus
+
 #include "processing_plant/processing_plant.hpp"
+
+#include <linux/i2c-dev.h>
+#include <linux/i2c.h>
+#include <sys/fcntl.h>
+#include <sys/ioctl.h>
 
 ProcessingPlant::ProcessingPlant(const rclcpp::NodeOptions& options)
     : Node("processing_plant", options)
 {
+    _init_i2c();
+    if (_check_sensor(_spectral_address) | _check_sensor(_magnetometer_address))
+    {
+        return;
+    }
+    _init_spectral();
+    _init_magnetometer();
     RCLCPP_INFO(this->get_logger(), "Processing plant node initialised");
+}
+
+void ProcessingPlant::_init_i2c()
+{
+    _i2c_file = open(_i2c_filename.c_str(), O_RDWR, O_APPEND);
+    if (_i2c_file < 0)
+    {
+        RCLCPP_FATAL(this->get_logger(), "Failed to open i2c path");
+    }
+    else
+    {
+        RCLCPP_DEBUG(this->get_logger(), "Opened i2c bus at device: %s", _i2c_filename.c_str());
+    }
+}
+
+int ProcessingPlant::_check_sensor(const uint16_t address)
+{
+    unsigned long functions = 0;
+    if (ioctl(_i2c_file, I2C_FUNCS, &functions) < 0)
+    {
+        RCLCPP_ERROR(this->get_logger(), "Error while sending 'get function' message to i2c address %d", address);
+        return errno;
+    }
+    if (functions & I2C_FUNC_I2C)
+    {
+        RCLCPP_INFO(this->get_logger(), "Device at address %d can be used with I2C_RDWR, continuing", address);
+        return 0;
+    }
+    else
+    {  // I didn't want to have to implement the lower-level ways of doing stuff if I didn't have to. I will remove this function if both sensors behave
+        RCLCPP_ERROR(this->get_logger(), "Device at address %d can NOT be used with I2C_RDWR", address);
+        return 1;
+    }
+}
+
+void ProcessingPlant::_init_spectral()
+{
+    std::vector<uint8_t> data = {_spectral_enable_register, _spectral_enable_data};
+    _write_i2c(_spectral_address, &data);
+    RCLCPP_DEBUG(this->get_logger(), "Spectral sensor initialised");
+}
+void ProcessingPlant::_init_magnetometer()
+{
+    std::vector<uint8_t> data = {_magnetometer_enable_data};
+    _write_i2c(_magnetometer_address, &data);
+    RCLCPP_DEBUG(this->get_logger(), "Magnetometer initialised");
+}
+
+void ProcessingPlant::_write_i2c(const uint16_t address, std::vector<uint8_t>* data)
+{
+    i2c_msg message[] = {{
+        .addr = address,
+        .flags = 0,
+        .len = static_cast<uint16_t>(data->size()),
+        .buf = data->data(),
+    }};
+    i2c_rdwr_ioctl_data send_message = {
+        .msgs = message,
+        .nmsgs = 1,
+    };
+    ioctl(_i2c_file, I2C_RDWR, &send_message);
+}
+
+std::vector<uint8_t> ProcessingPlant::_read_write_i2c(const uint16_t address, std::vector<uint8_t>* send_data, uint16_t read_bytes)
+{
+    uint8_t* received_data = (uint8_t*)malloc(read_bytes * sizeof(uint8_t));
+    i2c_msg messages[] = {
+        {.addr = address,
+         .flags = 0,
+         .len = static_cast<uint16_t>(send_data->size()),
+         .buf = send_data->data()},
+        {.addr = address,
+         .flags = I2C_M_RD,
+         .len = read_bytes,
+         .buf = received_data}};
+    i2c_rdwr_ioctl_data send_messages = {
+        .msgs = messages,
+        .nmsgs = sizeof(messages) / sizeof(messages[0]),
+    };
+    if (ioctl(_i2c_file, I2C_RDWR, &send_messages) < 0)
+    {
+        RCLCPP_ERROR(this->get_logger(), "Error reading from and writing to i2c bus");
+        return std::vector<uint8_t>{};
+    }
+    std::vector<uint8_t> return_data;
+    return_data.assign(received_data, (received_data + (read_bytes * sizeof(uint8_t))));
+    free(received_data);
+    return return_data;
+}
+
+ProcessingPlant::~ProcessingPlant()
+{
+    if (_i2c_file >= 0)
+    {
+        if (close(_i2c_file) < 0)
+        {
+            RCLCPP_FATAL(this->get_logger(), "Failed to close i2c bus at device: %s", _i2c_filename.c_str());
+        }
+    }
 }
 
 int main(int argc, char** argv)
