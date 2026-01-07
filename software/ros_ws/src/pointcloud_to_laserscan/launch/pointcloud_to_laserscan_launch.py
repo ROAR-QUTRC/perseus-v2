6475dfe751ea2a542b05d473c2b9515d245e64dd
LucaLubrano <lucalub123@gmail.com>
2026-01-07 16:03:13 +1000

    feat/lidar_conversion

---
+++ software/ros_ws/src/pointcloud_to_laserscan/launch/pointcloud_to_laserscan_launch.py
@@ -0,0 +1,38 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                name="scanner",
+                default_value="scanner",
+                description="converts lidar pointcloud to scan",
+            ),
+            Node(
+                package="pointcloud_to_laserscan",
+                executable="pointcloud_to_laserscan_node",
+                remappings=[
+                    ("cloud_in", "/points"),
+                    ("scan", "/lidar_scan"),
+                ],
+                parameters=[
+                    {
+                        "target_frame": "laser_frame",
+                        "transform_tolerance": 0.1,
+                        "min_height": -10.0,
+                        "max_height": 0.25,
+                        "range_min": 1.0,
+                        "angle_increment": 0.0174533,
+                        "scan_time": 0.1,
+                        "range_max": 40.0,
+                        "use_inf": True,
+                        "inf_epsilon": 1.0,
+                    }
+                ],
+                name="pointcloud_to_laserscan",
+            ),
+        ]
+    )
