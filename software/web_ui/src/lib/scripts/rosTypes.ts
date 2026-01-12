688068502f39045bffe183964903ac431f405fa1
OMEN44 <91703934+OMEN44@users.noreply.github.com>
2026-01-12 12:56:13 +0000

    chore: Format and lint

--- software/web_ui/src/lib/scripts/rosTypes.ts
+++ software/web_ui/src/lib/scripts/rosTypes.ts
@@ -1,17 +1,17 @@
 // std_msgs types
 export interface Float64MultiArrayType {
-	data: Array<number>;
+  data: Array<number>;
 }
 
 // actuator_msgs types
 export interface ActuatorsMessageType {
-	header: {
-		stamp: object; // filled by rosbridge
-		frame_id: string;
-	};
-	position: Array<number>; // float64
-	velocity: Array<number>; // float64
-	normalized: Array<number>; // float64
+  header: {
+    stamp: object; // filled by rosbridge
+    frame_id: string;
+  };
+  position: Array<number>; // float64
+  velocity: Array<number>; // float64
+  normalized: Array<number>; // float64
 }
 
 // Service types
@@ -20,10 +20,10 @@ export interface EmptyRequestType {}
 export interface EmptyResponseType {}
 
 export interface SetBoolRequestType {
-	data: boolean;
+  data: boolean;
 }
 
 export interface SetBoolResponseType {
-	success: boolean;
-	message: string;
+  success: boolean;
+  message: string;
 }
