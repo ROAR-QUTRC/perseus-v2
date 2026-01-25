c8a596f301417f0329d6873784b52e41bd85d2cf
James N <59348282+RandomSpaceship@users.noreply.github.com>
2026-01-25 16:55:09 +1000

    docs(nix): Remove old outputs on setup

--- docs/nix/setup.nix
+++ docs/nix/setup.nix
@@ -11,6 +11,10 @@ writeShellScriptBin "roar-docs-setup" ''
   set -e
 
   source ${cd-docs-source}
+  # clean up generated figures + code docs
+  rm -rf "${output-dir}"
+  # and old Doxygen output
+  rm -rf "../build/doxygen"
   mkdir -p "${output-dir}"
   cp -a ${figures}/. "${output-dir}"
   # nix store is read-only by default, so fix that after copying
