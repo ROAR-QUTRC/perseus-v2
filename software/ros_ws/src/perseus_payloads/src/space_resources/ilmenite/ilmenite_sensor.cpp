61252463aa4702db32ff7c19518858a8be4b74cb
Conner <connerjs345@gmail.com>
2026-01-29 16:34:04 +1000

    Re-did Space Resources ROS nodes and directory structure. Excluding Kibisis nodes.

--- software/ros_ws/src/perseus_payloads/src/space_resources/ilmenite/ilmenite_sensor.cpp
+++
@@ -1,8 +0,0 @@
-#include "space_resources/ilmenite/ilmenite_sensor.hpp"
-
-IlmeniteSensor::IlmeniteSensor() {}
-
-void IlmeniteSensor::update()
-{
-  // logic
-}
---
+++ software/ros_ws/src/perseus_payloads/src/space_resources/space_resources_controller/main.cpp
@@ -0,0 +1,11 @@
+#include "space_resources/space_resources_controller/main.hpp"
+#include <memory>
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<SpaceResourcesController>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
