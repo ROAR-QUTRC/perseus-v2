850a25550125b94588e73c3c27e0dded38d05d5b
OMEN44 <huonswales@gmail.com>
2026-01-30 13:05:18 +1000

    Feat: fixed all web ui errors and updated packages

---
+++ software/web_ui/src/lib/widgets/armControl.svelte
@@ -0,0 +1,388 @@
+<script lang="ts" module>
+	// This is to expose the widget settings to the panel. Code in here will only run once when the widget is first loaded.
+	import type { WidgetGroupType, WidgetSettingsType } from '$lib/scripts/state.svelte';
+
+	export const name = 'Arm Controller';
+	// These properties are optional
+	export const description = 'Controller for each of the post landing arm servos';
+	export const group: WidgetGroupType = 'ROS';
+	export const isRosDependent = true; // Set to true if the widget requires a ROS connection
+
+	export const settings: WidgetSettingsType = $state<WidgetSettingsType>({
+		groups: {
+			general: {
+				bigIncrementValue: {
+					type: 'number',
+					value: '30'
+				},
+				smallIncrementValue: {
+					type: 'number',
+					value: '5'
+				},
+				automaticallySendMessages: {
+					description: 'Disables automatic sending of control messages',
+					type: 'switch',
+					value: 'false'
+				},
+				messageFrequency: {
+					description: 'Frequency (Hz) to send control messages at',
+					type: 'number',
+					value: '10'
+				}
+			}
+		}
+	});
+</script>
+
+<script lang="ts">
+	import { getRosConnection } from '$lib/scripts/rosBridge.svelte'; // ROSLIBJS docs here: https://robotwebtools.github.io/roslibjs/Service.html
+	import * as ButtonGroup from '$lib/components/ui/button-group/index';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Input from '$lib/components/ui/input/input.svelte';
+	import ScrollArea from '$lib/components/ui/scroll-area/scroll-area.svelte';
+	import type { RequestInt8ArrayResponseType, TriggerDeviceRequestType, TriggerDeviceResponseType } from '$lib/scripts/perseusMsgs';
+	import type { EmptyRequestType, Float64MultiArrayType, SetBoolRequestType, SetBoolResponseType } from '$lib/scripts/rosTypes';
+	import { faChevronDown, faChevronUp, faRefresh } from '@fortawesome/free-solid-svg-icons';
+	import { sentenceCase } from 'change-case';
+	import * as ROSLIB from 'roslib';
+	import { onMount, untrack } from 'svelte';
+	import Fa, { FaLayers } from 'svelte-fa';
+
+
+	interface MotorData {
+		rmdMotor: boolean;
+		showDirectPosition: boolean;
+		targetAngle: number;
+		draftTargetAngle: number;
+		angle: number;
+		rpm: number;
+		temperature: number;
+		current: number;
+		voltage: number;
+		gearRatio: Readonly<number>;
+		error: string;
+	}
+
+	// ---------------------------
+	//			ROS LOGIC
+	// ---------------------------
+
+	let interval: NodeJS.Timeout | null = null;
+	let automaticPositionMessages = $derived(settings.groups.general.automaticallySendMessages.value === 'true');
+	let debugEnabled = $state(false);
+	let motorControlTopic: ROSLIB.Topic<Float64MultiArrayType> | null = null;
+	let motorStatusTopic: ROSLIB.Topic<Float64MultiArrayType> | null = null;
+	let enableDebugStatsService: ROSLIB.Service<SetBoolRequestType, SetBoolResponseType> | null = null;
+	let setIdService: ROSLIB.Service<TriggerDeviceRequestType, TriggerDeviceResponseType> | null = null;
+	let setZeroPositionService: ROSLIB.Service<TriggerDeviceRequestType, TriggerDeviceResponseType> | null = null;
+	let restartMotorService: ROSLIB.Service<TriggerDeviceRequestType, TriggerDeviceResponseType> | null = null;
+	let getCanIdsService: ROSLIB.Service<EmptyRequestType, RequestInt8ArrayResponseType> | null = null;
+	// ROS2 connection
+	$effect(() => {
+		const ros = getRosConnection();
+		if (ros) {
+			motorControlTopic = new ROSLIB.Topic({
+				ros: ros,
+				name: '/arm/control',
+				messageType: 'std_msgs/msg/Float64MultiArray'
+			});
+			motorStatusTopic = new ROSLIB.Topic({
+				ros: ros,
+				name: '/arm/status',
+				messageType: 'std_msgs/msg/Float64MultiArray'
+			});
+			motorStatusTopic.subscribe(onStatusMessage)
+			enableDebugStatsService = new ROSLIB.Service({
+				ros: ros,
+				name: '/arm/rmd/enable_debug_stats',
+				serviceType: 'std_srvs/srv/SetBool'
+			});
+			setIdService = new ROSLIB.Service({
+				ros: ros,
+				name: '/arm/rmd/set_id',
+				serviceType: 'perseus_msgs/srv/TriggerDevice'
+			});
+			setZeroPositionService = new ROSLIB.Service({
+				ros: ros,
+				name: '/arm/rmd/set_zero_position',
+				serviceType: 'perseus_msgs/srv/TriggerDevice'
+			});
+			restartMotorService = new ROSLIB.Service({
+				ros: ros,
+				name: '/arm/rmd/restart_motor',
+				serviceType: 'perseus_msgs/srv/TriggerDevice'
+			});
+			getCanIdsService = new ROSLIB.Service({
+				ros: ros,
+				name: '/arm/rmd/get_can_ids',
+				serviceType: 'perseus_msgs/srv/RequestInt8Array'
+			});
+			getAllIds();
+		} else {
+			motorControlTopic = null;
+		}
+	});
+	
+	// Handle changes to settings
+	$effect(() => {
+		if (!automaticPositionMessages && interval) {
+			clearInterval(interval);
+			interval = null;
+		}
+		untrack(() => {
+			settings.groups.general.messageFrequency.disabled = !automaticPositionMessages;
+			if (automaticPositionMessages) {
+				const frequency = Number(settings.groups.general.messageFrequency.value);
+				interval = setInterval(() => {
+					sendPositions();
+				}, 1000 / frequency);
+			}
+		});
+	});
+	
+	const sendPositions = () => {
+		if (motorControlTopic) {
+			// save draft targets to targets
+			joints.forEach((joint) => {
+				motors[joint].targetAngle = motors[joint].draftTargetAngle;
+			});
+			const data = [0, 0, 0, 0, 0];
+			Object.keys(jointIdMap).forEach((joint) => {
+				const jointName = joint as JointNameType;
+				data[jointIdMap[jointName] - 1] = motors[jointName].targetAngle * (motors[jointName].showDirectPosition ? 1 : motors[jointName].gearRatio);
+			})
+			const message = {
+				// Index of this array corresponds to motor ID - 1
+				data
+			};
+			motorControlTopic.publish(message);
+		}
+	}
+
+	const onStatusMessage = (message: Float64MultiArrayType) => {
+		// message.data is an array of floats in the order:
+		// [motor_id, error_code, temperature, voltage, current, rpm, angle, phase_<a,b,c>_current]
+		// Reshape data (Each motor sends 10 values)
+		const data: Array<Array<number>> = [];
+		for (let i = 0; i < message.data.length; i += 10) {
+			data.push(message.data.slice(i, i + 10));
+		}
+		for (const dataSet of data) {
+			const jointName = Object.keys(jointIdMap).find(key => jointIdMap[key as JointNameType] === dataSet[0]) as JointNameType;
+			if (jointName) {
+				motors[jointName].error = 'None';
+				motors[jointName].temperature = dataSet[2];
+				motors[jointName].voltage = dataSet[3];
+				motors[jointName].current = dataSet[4];
+				motors[jointName].rpm = dataSet[5];
+				motors[jointName].angle = dataSet[6] / (motors[jointName].showDirectPosition ? 1 : motors[jointName].gearRatio);
+			}
+		}
+	}
+
+	const onEnableDebug = () => {
+		if (enableDebugStatsService) {
+			enableDebugStatsService.callService({ data: !debugEnabled }, (response) => {
+				debugEnabled = !debugEnabled;
+			});
+		}
+	}
+
+	// ---------------------------
+	//			UI LOGIC
+	// ---------------------------
+
+	type JointNameType = typeof joints[number];
+	
+	const joints = ['SHOULDER_PAN', 'SHOULDER_TILT', 'ELBOW', 'WRIST_PAN', 'WRIST_TILT'] as const;
+	const jointIdMap: Record<JointNameType, number> = {
+		WRIST_TILT: 1,
+		WRIST_PAN: 2,
+		ELBOW: 3,
+		SHOULDER_TILT: 4,
+		SHOULDER_PAN: 5,
+	};
+	let motors = $state<Record<JointNameType, MotorData>>(
+		{} as Record<JointNameType, MotorData>
+	);
+	let onlineMotors = $state<Array<number>>([])
+	let bigIncrement = $derived(Number(settings.groups.general.bigIncrementValue.value));
+	let smallIncrement = $derived(Number(settings.groups.general.smallIncrementValue.value));
+
+	const toggleDirectOutput = (motorId: JointNameType, directButton: boolean) => {
+		if (motors[motorId].showDirectPosition && directButton) return;
+		if (!motors[motorId].showDirectPosition && !directButton) return;
+		motors[motorId].showDirectPosition = !motors[motorId].showDirectPosition;
+		const scale = motors[motorId].showDirectPosition ? motors[motorId].gearRatio : (1 / motors[motorId].gearRatio);
+		motors[motorId].targetAngle = motors[motorId].targetAngle * scale;
+		motors[motorId].draftTargetAngle = motors[motorId].draftTargetAngle * scale;
+		motors[motorId].angle = motors[motorId].angle * scale;
+	};
+
+	const setTargetAngle = (joint: JointNameType, angle?: number) => {
+		motors[joint].targetAngle = angle ?? motors[joint].draftTargetAngle;
+		motors[joint].draftTargetAngle = motors[joint].targetAngle;
+	}
+
+	const setCurrentToZero = (joint: JointNameType) => {
+		if (setZeroPositionService) {
+			setZeroPositionService.callService({ id: jointIdMap[joint], trigger: true, data: 0 }, (response) => {
+				// Do nothing for now
+			});
+		}
+	}
+
+	const incrementDraftTarget = (joint: JointNameType, increment: number) => {
+		motors[joint].draftTargetAngle += increment;
+	}
+
+	const getAllIds = () => {
+		if (getCanIdsService) {
+			getCanIdsService.callService({}, (response) => {
+				onlineMotors = response.data;
+			})
+		}
+	}
+
+	let getMotorIdInterval: NodeJS.Timeout | null = null;
+
+	onMount(() => {
+		// Initialize motor data
+		joints.forEach((joint) => {
+			motors[joint] = {
+				rmdMotor: true,
+				showDirectPosition: false,
+				targetAngle: 0,
+				draftTargetAngle: 0,
+				angle: 0,
+				rpm: 0,
+				temperature: 0,
+				current: 0,
+				voltage: 0,
+				gearRatio: 10,
+				error: 'None'
+			};
+		});
+		// Set correct gear ratios
+		motors.ELBOW.gearRatio = 28;
+		motors.WRIST_PAN.gearRatio = 19;
+		motors.WRIST_TILT.gearRatio = 28;
+		motors.SHOULDER_PAN.gearRatio = 6;
+		motors.SHOULDER_PAN.rmdMotor = false;
+		motors.SHOULDER_TILT.gearRatio = 6;
+		motors.SHOULDER_TILT.rmdMotor = false;
+
+
+		// ensure connected motors is updated regularly
+		getMotorIdInterval = setInterval(getAllIds, 1000);
+
+		return () => {
+			if (interval) {
+				clearInterval(interval);
+				interval = null;
+			}
+			if (getMotorIdInterval) {
+				clearInterval(getMotorIdInterval);
+				getMotorIdInterval = null;
+			}
+		};
+	})
+</script>
+
+<div class="w-full h-full flex flex-col">
+	<div class="flex items-center w-full pb-2 gap-2 px-4">
+		<Button disabled={automaticPositionMessages} onclick={sendPositions}>Send Angles</Button>
+		<Button onclick={onEnableDebug}>{debugEnabled ? 'Disable' : 'Enable'} Debug</Button>
+		<p class="ml-auto">Available Motor IDs: {onlineMotors.length > 0 ? onlineMotors.join(', ') : 'None'}</p>
+		<Button class=" rounded-full" variant="ghost" size="icon"><Fa icon={faRefresh} /></Button>
+	</div>
+	<ScrollArea class="h-full w-full pr-2 flex-1">
+		<div class="h-full w-full flex flex-wrap gap-x-2">
+			{#each Object.keys(motors) as motor}
+				{@const data = motors[motor as JointNameType]}
+				{@const id = motor as JointNameType}
+				<div class="border rounded-2xl mb-2 w-fit h-fit flex flex-col p-2">
+					<p class="text-xl text-center mb-2">{sentenceCase(motor)} <span class="opacity-50">(id: {jointIdMap[id]})</span></p>
+					<div class="flex gap-2" class:disabled={!onlineMotors.includes(jointIdMap[id])}>
+						<div>
+							<!-- Gage -->
+							<div class="border aspect-square rounded-[50%] text-center flex flex-col justify-center relative">
+								<p>Angle: {data.angle.toFixed(1)}&deg;</p>
+								<p>RPM: {data.rpm.toFixed(1)}</p>
+								<p>Temp: {data.temperature.toFixed(1)} &deg;C</p>
+								<span class="absolute border border-l-0 border-r left-1/2 top-0 h-[5%] translate-x-[-1px]"></span>
+								<span class="absolute border border-l-0 border-r left-1/2 bottom-0 h-[5%] translate-x-[-1px]"></span>
+								<span class="absolute border border-b-0 border-t top-1/2 w-[5%] translate-x-[-1px]"></span>
+								<span class="absolute border border-b-0 border-t top-1/2 right-0 w-[5%] translate-x-[-1px]"></span>
+								<svg viewBox="0 0 100 100" class="w-full h-full absolute top-0 left-0 overflow-visible">
+									<circle
+										r="3%"
+										cx={`${50 * Math.cos(((data.angle) * -1 - 90) * Math.PI / 180) + 50}%`}
+										cy={`${50 * Math.sin(((data.angle) * -1 - 90) * Math.PI / 180) + 50}%`}
+										fill="hsl(20.5 90.2% 48.2%)"
+									/>
+									<circle 
+										r="3%"
+										cx={`${50 * Math.cos((data.targetAngle * -1 - 90) * Math.PI / 180) + 50}%`}
+										cy={`${50 * Math.sin((data.targetAngle * -1 - 90) * Math.PI / 180) + 50}%`}
+										fill="hsl(20.5 90.2% 48.2%)"
+										opacity="0.3"
+										class="transition-all"
+									/>
+								</svg>
+							</div>
+							<ButtonGroup.Root class="mt-2">
+								<Button class="border" 
+										variant={data.showDirectPosition ? 'default' : 'outline'} 
+										onclick={() => toggleDirectOutput(id, true)}>
+									Direct
+								</Button>
+								<Button class="border" 
+										variant={data.showDirectPosition ? 'outline' : 'default'} 
+										onclick={() => toggleDirectOutput(id, false)}>
+									Output
+								</Button>
+							</ButtonGroup.Root>
+							<p class="opacity-40 text-center">Gear ratio: 1:{data.gearRatio}</p>
+						</div>
+						<div>
+							<!-- Controller -->
+							<ButtonGroup.Root orientation="vertical" class="max-w-[5rem]" >
+								<Button variant="outline" onclick={() => incrementDraftTarget(id, bigIncrement)}>
+									<FaLayers pull="left">
+										<Fa translateY={0} icon={faChevronUp} />
+										<Fa translateY={0.4} icon={faChevronUp} />
+									</FaLayers>
+								</Button>
+								<Button variant="outline" onclick={() => incrementDraftTarget(id, smallIncrement)}><Fa icon={faChevronUp} /></Button>
+								<Input type="number" max={32767} min={-32767} class="focus-visible:ring-1 z-10 text-center pr-0" bind:value={data.draftTargetAngle}/>
+								<Button variant="outline" onclick={() => incrementDraftTarget(id, -smallIncrement)}><Fa icon={faChevronDown} /></Button>
+								<Button variant="outline" onclick={() => incrementDraftTarget(id, -bigIncrement)}>
+									<FaLayers pull="left">
+										<Fa translateY={0} icon={faChevronDown} />
+										<Fa translateY={-0.4} icon={faChevronDown} />
+									</FaLayers>
+								</Button>
+							</ButtonGroup.Root>
+						</div>
+						<div class="flex flex-col">
+							<!-- Extra stats -->
+							<Button variant="outline" onclick={() => setTargetAngle(id, 0)}>Zero Servo</Button>
+							<Button disabled={!motors[id].rmdMotor} class="mt-2" onclick={() => setCurrentToZero(id)}>Write Zero Pos</Button>
+							<p>Current: {data.current.toFixed(2)} A</p>
+							<p>Voltage: {data.voltage.toFixed(2)} V</p>
+							<p>Error: {data.error}</p>
+						</div>
+					</div>
+				</div>
+			{/each}
+		</div>
+	</ScrollArea>
+</div>
+
+<style>
+	.disabled {
+		opacity: 0.5;
+		pointer-events: none;
+	}
+</style>
---
+++ software/web_ui/src/lib/widgets/rmdConfig.svelte
@@ -0,0 +1,331 @@
+<script lang="ts" module>
+	// This is to expose the widget settings to the panel. Code in here will only run once when the widget is first loaded.
+	import type { WidgetGroupType, WidgetSettingsType } from '$lib/scripts/state.svelte';
+
+	export const name = 'RMD Config Widget';
+	// These properties are optional
+	export const description = 'Configure and monitor RMD motors connected via CAN';
+	export const group: WidgetGroupType = 'CAN Bus';
+	export const isRosDependent = true; // Set to true if the widget requires a ROS connection
+
+	export const settings: WidgetSettingsType = $state<WidgetSettingsType>({
+		groups: {}
+	});
+</script>
+
+<script lang="ts">
+	import { getRosConnection } from '$lib/scripts/rosBridge.svelte'; // ROSLIBJS docs here: https://robotwebtools.github.io/roslibjs/Service.html
+	import * as ROSLIB from 'roslib';
+	import { onMount } from 'svelte';
+	import * as Select from '$lib/components/ui/select';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Fa from 'svelte-fa';
+	import { faGaugeHigh, faRefresh, faTemperature0 } from '@fortawesome/free-solid-svg-icons';
+	import ScrollArea from '$lib/components/ui/scroll-area/scroll-area.svelte';
+	import Input from '$lib/components/ui/input/input.svelte';
+	import type { RmdBrakeRequest, RmdCanIdResponse, RmdDataRequest, RMDStatusRequest, RMDStatusResponse, SuccessServiceResponse as SuccessResponse } from './rmdConfig/serviceTypes';
+	import * as AlertDialog from '$lib/components/ui/alert-dialog';
+
+	const errorConstants: Record<number, string> = {
+		0x0000: 'NO_ERROR',
+		0x0002: 'STALL',
+		0x0004: 'LOW_VOLTAGE',
+		0x0008: 'OVER_VOLTAGE',
+		0x0010: 'OVER_CURRENT',
+		0x0040: 'POWER_OVERRUN',
+		0x0080: 'CALIBRATION_PARAMETER_WRITE',
+		0x0100: 'SPEEDING',
+		0x1000: 'OVER_TEMPERATURE',
+		0x2000: 'ENCODER_CALIBRATION'
+	};
+
+	const getErrorList = (errorCode: number): string[] => {
+		const errors: string[] = [];
+		for (const [code, description] of Object.entries(errorConstants)) {
+			if ((errorCode & Number(code)) !== 0) {
+				errors.push(description);
+			}
+		}
+		return errors;
+	};
+
+	let enableStatusService: ROSLIB.Service<{ data: boolean }, { success: boolean }> | null = null;
+	let statusService: ROSLIB.Service<RMDStatusRequest, RMDStatusResponse> | null = null;
+	let canIdService: ROSLIB.Service<{}, RmdCanIdResponse> | null = null;
+	let brakeEnableService: ROSLIB.Service<RmdBrakeRequest, SuccessResponse> | null = null;
+	let restartMotorService: ROSLIB.Service<RmdDataRequest, SuccessResponse> | null = null;
+	let setIdService: ROSLIB.Service<RmdDataRequest, SuccessResponse> | null = null;
+	let zeroMotorService: ROSLIB.Service<RmdDataRequest, SuccessResponse> | null = null;
+
+	let motorStatus = $state<RMDStatusResponse | null>(null);
+	let ids = $state<Array<number>>([]);
+	let selectedIdInput = $state<string>('0');
+	let selectedId = $derived(Number(selectedIdInput));
+	let debugEnabled = $state<boolean>(false);
+
+	// Widget logic goes here
+	$effect(() => {
+		const ros = getRosConnection();
+
+		if (ros) {
+			statusService = new ROSLIB.Service({
+				ros: ros,
+				name: '/arm/rmd_status',
+				serviceType: 'perseus_msgs/srv/RmdServoStatus'
+			});
+
+			canIdService = new ROSLIB.Service({
+				ros: ros,
+				name: '/arm/rmd_can_ids',
+				serviceType: 'perseus_msgs/srv/RmdCanId'
+			});
+
+			brakeEnableService = new ROSLIB.Service({
+				ros: ros,
+				name: '/arm/rmd_set_brake',
+				serviceType: 'perseus_msgs/srv/RmdBrake'
+			});
+
+			restartMotorService = new ROSLIB.Service({
+				ros: ros,
+				name: '/arm/rmd_restart_motor',
+				serviceType: 'perseus_msgs/srv/RmdData'
+			});
+
+			enableStatusService = new ROSLIB.Service({
+				ros: ros,
+				name: '/arm/rmd_enable_status_messages',
+				serviceType: 'std_srvs/srv/SetBool'
+			});
+
+			setIdService = new ROSLIB.Service({
+				ros: ros,
+				name: '/arm/rmd_set_motor_id',
+				serviceType: 'perseus_msgs/srv/RmdData'
+			});
+
+			zeroMotorService = new ROSLIB.Service({
+				ros: ros,
+				name: '/arm/rmd_set_zero_position',
+				serviceType: 'perseus_msgs/srv/RmdData'
+			});
+
+			callGetIds();
+		} else {
+			statusService = null;
+			canIdService = null;
+			brakeEnableService = null;
+			restartMotorService = null;
+			enableStatusService = null;
+			setIdService = null;
+			zeroMotorService = null;
+		}
+	});
+
+	const getMotorStatus = () => {
+		if (statusService && selectedId > 0) {
+			const request: RMDStatusRequest = {
+				motor_id: selectedId
+			};
+
+			statusService.callService(request, (response: RMDStatusResponse) => {
+				motorStatus = response;
+			});
+		}
+	};
+
+	const callGetIds = () => {
+		if (canIdService) {
+			canIdService.callService({}, (response: RmdCanIdResponse) => {
+				ids = [0, ...response.servo_ids];
+			});
+		}
+	}
+
+	const callBrakeEnable = (enable: boolean) => {
+		if (brakeEnableService) {
+			const request = {
+				motor_id: selectedId,
+				brake_enable: enable
+			};
+
+			brakeEnableService.callService(request, (response: { success: boolean }) => {
+				console.log(`Brake ${enable ? 'enabled' : 'disabled'} for motor ID ${selectedId}: ${response.success}`);
+			});
+		}
+	};
+
+	const callDebugEnable = () => {
+		if (enableStatusService) {
+			const request = {
+				data: !debugEnabled
+			};
+
+			enableStatusService.callService(request, () => {
+				debugEnabled = !debugEnabled;
+			});
+		}
+	};
+
+	const restartMotor = () => {
+		if (restartMotorService) {
+			const request: RmdBrakeRequest = {
+				motor_id: selectedId,
+			};
+
+			restartMotorService.callService(request, (response: { success: boolean }) => {
+				console.log(`Restart command sent for motor ID ${selectedId}: ${response.success}`);
+			});
+		}
+	};
+
+	let zeroBias = $state<number>(0);
+	const setMotorZero = (bias?: number) => {
+		if (zeroMotorService) {
+			const request: RmdDataRequest = {
+				motor_id: selectedId,
+				trigger: !bias, // true is settings zero to current position
+				data: bias ?? 0 // Data field is unused for this service
+			};
+
+			if (bias) zeroBias = 0;
+
+			zeroMotorService.callService(request, (response: { success: boolean }) => {
+				console.log(`Zero position command sent for motor ID ${selectedId}: ${response.success}`);
+			});
+		}
+	};
+
+	let newMotorId = $state<number>(0);
+	let setIdConfirm = $state<boolean>(false);
+	// id can be between 1 and 32 inclusive
+	const isMotorIdValid = $derived(newMotorId > 0 && newMotorId <= 32);
+	const setMotorId = () => {
+		if (isMotorIdValid && setIdService) {
+			const request: RmdDataRequest = {
+				motor_id: selectedId,
+				data: newMotorId
+			};
+
+			setIdService.callService(request, (response: { success: boolean }) => {
+				ids = [];
+				selectedId = 0;
+				callGetIds();
+				setIdConfirm = false;
+			});
+		}
+	}
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			getMotorStatus();
+		}, 100);
+
+		return () => clearInterval(interval);
+	});
+	
+</script>
+
+<div class="flex">
+	<strong class="my-auto text-xl">Motor ID:</strong>
+	<div class="mx-2 w-[60px]">
+		<Select.Root type="single" bind:value={selectedIdInput}>
+			<Select.Trigger>
+				{selectedId === 0 ? 'All' : selectedId}
+			</Select.Trigger>
+			<Select.Content>
+				{#each ids as id}
+				<Select.Item value={id.toString()}>{id === 0 ? 'All' : id}</Select.Item>
+				{/each}
+			</Select.Content>
+		</Select.Root>
+	</div>
+	<Button size="icon" variant="outline" onclick={callGetIds}><Fa icon={faRefresh} /></Button>
+	<Button class="ml-auto" onclick={callDebugEnable}>{debugEnabled ? 'Disable ' : 'Enable '} Debug</Button>
+</div>
+
+<div class="flex gap-2 mt-2 h-full flex-wrap">
+	<div class="min-w-[200px] mb-2 w-1/4">
+		<div class="border aspect-square w-full rounded-2xl relative">
+			{#if selectedId !== 0}
+				<div class="text-center content-center absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-2/3 aspect-square">
+					<p><strong>Temp</strong>: {motorStatus?.temperature.toFixed(2)}&deg;C</p>
+					<p><strong>RPM</strong>: {motorStatus?.speed.toFixed(2)} dps</p>
+					<p><strong>Volts</strong>: {motorStatus?.voltage.toFixed(2)} V</p>
+					<p><strong>Amps</strong>: {motorStatus?.torque_current.toFixed(2)} A</p>
+					<p><strong>Angle</strong>: {motorStatus?.angle.toFixed(2)}&deg;</p>
+				</div>
+				<div class="z-5 absolute aspect-square w-[80%] top-[10%] left-[10%] border rounded-[50%]">
+					<span class="absolute border border-l-0 border-r left-1/2 h-[5%] translate-x-[-1px]"></span>
+					<span class="absolute border border-l-0 border-r left-1/2 bottom-0 h-[5%] translate-x-[-1px]"></span>
+					<span class="absolute border border-b-0 border-t top-1/2 w-[5%] translate-x-[-1px]"></span>
+					<span class="absolute border border-b-0 border-t top-1/2 right-0 w-[5%] translate-x-[-1px]"></span>
+				</div>
+				<svg viewBox="0 0 100 100" class="w-full h-full absolute top-0 left-0 z-10">
+					<circle 
+						r="3%"
+						cx={`${40 * Math.cos((motorStatus?.angle! * -1 - 90) * Math.PI / 180) + 50}%`}
+						cy={`${40 * Math.sin((motorStatus?.angle! * -1 - 90) * Math.PI / 180) + 50}%`}
+						fill="red"
+					/>
+				</svg>
+			{:else}
+				<div class="text-center content-center absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-1/2 aspect-square">
+					<p>Select a single motor ID to view status</p>
+				</div>
+			{/if}
+		</div>
+	</div>
+	<div class="w-1/2 min-w-[400px]">
+		<div style:aspect-ratio="2 / 1" class="w-full border rounded-2xl p-2 flex flex-col relative">
+			{#if selectedId !== 0}
+				<p class="">Change Motor ID:</p>
+				<div class="flex gap-2 mb-2">
+					<Input bind:value={newMotorId} type="number" />
+					<AlertDialog.Root bind:open={setIdConfirm}>
+						<AlertDialog.Trigger>
+							<Button disabled={!isMotorIdValid}>Set</Button>
+						</AlertDialog.Trigger>
+						<AlertDialog.Content>
+							<AlertDialog.Header>
+								<AlertDialog.Title>
+									Caution: This will restart all connected motors
+								</AlertDialog.Title>
+								<AlertDialog.Description>
+									This will disengage <strong>all</strong> motors breaks. You must ensure they are not under load.
+								</AlertDialog.Description>
+							</AlertDialog.Header>
+							<AlertDialog.Footer>
+								<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+								<AlertDialog.Action onclick={setMotorId}>Confirm</AlertDialog.Action>
+							</AlertDialog.Footer>
+						</AlertDialog.Content>
+					</AlertDialog.Root>
+				</div>
+				<p class="">Set Zero Bias:</p>
+				<div class="flex gap-2 mb-2">
+					<Input bind:value={zeroBias} type="number" />
+					<Button onclick={() => setMotorZero(zeroBias)}>Set</Button>
+					<Button onclick={() => setMotorZero()}>Set Current as Zero</Button>
+				</div>
+				<Button disabled={selectedId === 0} variant="outline" onclick={restartMotor}>Restart</Button>
+			{:else}
+				<div class="text-center content-center absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-1/2 aspect-square">
+					<p>Select a single motor ID to view status</p>
+				</div>
+			{/if}
+		</div>
+	</div>
+	<div class="min-w-[200px] mb-2">
+		<p><strong>Motor ID</strong>: {motorStatus?.motor_id}</p>
+		<p><strong>Brake engaged</strong>: {motorStatus?.brake_control ? 'Yes' : 'No'}</p>
+		<p><strong>Phase current</strong>:<br />a -> {motorStatus?.phase_a_current.toFixed(2)}, <br />b -> {motorStatus?.phase_b_current.toFixed(2)},<br />c -> {motorStatus?.phase_c_current.toFixed(2)}</p>
+		<p><strong>Errors</strong>: {getErrorList(motorStatus?.error ?? 0).join(', ')}</p>
+	</div>
+</div>
+
+<!-- <p>ids: {ids}</p>
+<button onclick={callGetIds}>Get Ids</button>
+
+<p>{output}</p>
+<button onclick={callService}>Get RMD Status</button> -->
