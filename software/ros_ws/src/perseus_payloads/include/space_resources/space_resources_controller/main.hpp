83bc1e9529ca47b2a62a2959e0fdad5a2d2cb6f9
BrockMorgan88 <brockjamesmorgan@gmail.com>
2026-02-06 17:13:20 +1000

    feat: Initial publishers, subscribers, and services

--- software/ros_ws/src/perseus_payloads/include/space_resources/space_resources_controller/main.hpp
+++ software/ros_ws/src/perseus_payloads/include/space_resources/space_resources_controller/main.hpp
@@ -1,14 +1,21 @@
 #pragma once
 
-#include "rclcpp/rclcpp.hpp"
+#include <actuator_msgs/msg/actuators_angular_velocity.hpp>
+#include <perseus_interfaces/srv/concentration.hpp>
+#include <perseus_interfaces/srv/illuminance_array.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/bool.hpp>
 
 class SpaceResourcesController : public rclcpp::Node
 {
 public:
-    SpaceResourcesController()
-        : Node("space_resources_controller")
-    {
-        RCLCPP_INFO(this->get_logger(), "SpaceResourcesController node started");
-        // publishers, subscribers, etc
-    }
+    explicit SpaceResourcesController(const rclcpp::NodeOptions& options = rclcpp::NodeOptions());
+
+private:
+    void _speed_callback(const actuator_msgs::msg::ActuatorsAngularVelocity msg);
+    rclcpp::Subscription<actuator_msgs::msg::ActuatorsAngularVelocity>::SharedPtr _centrifuge_speed_callback;
+    rclcpp::Client<perseus_interfaces::srv::Concentration>::SharedPtr _water_concentration_service;
+    rclcpp::Client<perseus_interfaces::srv::Concentration>::SharedPtr _ilmenite_concentration_service;
+    rclcpp::Publisher<actuator_msgs::msg::ActuatorsAngularVelocity>::SharedPtr _centrifuge_control_publisher;
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr _centrifuge_brake_publisher;
 };
