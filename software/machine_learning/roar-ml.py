22a912d8c6354732515ccabf985ef320584ae5ee
DanB <jdbretherton12@gmail.com>
2026-02-09 17:51:47 +1000

    feat: More nix things

--- software/machine_learning/roar-ml.py
+++
@@ -1,9 +0,0 @@
-import roar_ml
-
-
-def main():
-    print("Imported Torch")
-
-
-if __name__ == "__main__":
-    main()
--- software/machine_learning/setup.py
+++ software/machine_learning/setup.py
@@ -1,12 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name="roar-ml",
+    name="roar_ml",
     version="0.0.1",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    py_modules=["roar_ml"],
     install_requires=[
         "torch",
     ],
     python_requires=">=3.8",
+    entry_points={
+        "console_scripts": [
+            "roar_ml=roar_ml:main",
+        ],
+    },
 )
