5db43fa9a3624fc31d31d25383a247067727e6b7
RandomSpaceship <59348282+RandomSpaceship@users.noreply.github.com>
2026-02-16 09:13:10 +0000

    chore: Update Nix packaging

--- software/ros_ws/nix-packages/overlay.nix
+++ software/ros_ws/nix-packages/overlay.nix
@@ -16,6 +16,7 @@ final: prev: {
   perseus-payloads = final.callPackage ./perseus-payloads.nix { };
   perseus-sensors = final.callPackage ./perseus-sensors.nix { };
   perseus-simulation = final.callPackage ./perseus-simulation.nix { };
+  perseus-text-detection = final.callPackage ./perseus-text-detection.nix { };
   pointcloud-to-laserscan = final.callPackage ./pointcloud-to-laserscan.nix { };
   teleop-diagnostics = final.callPackage ./teleop-diagnostics.nix { };
 }
