db3c9d37b379521557fd93edb8834141304e4922
RandomSpaceship <59348282+RandomSpaceship@users.noreply.github.com>
2026-02-20 04:56:16 +0000

    chore: Update Nix packaging

--- software/ros_ws/nix-packages/overlay.nix
+++ software/ros_ws/nix-packages/overlay.nix
@@ -1,6 +1,7 @@
 final: prev: {
   autonomy = final.callPackage ./autonomy.nix { };
   input-devices = final.callPackage ./input-devices.nix { };
+  mapping-autotune = final.callPackage ./mapping-autotune.nix { };
   perseus = final.callPackage ./perseus.nix { };
   perseus-autonomy-bridge = final.callPackage ./perseus-autonomy-bridge.nix { };
   perseus-bt-nodes = final.callPackage ./perseus-bt-nodes.nix { };
