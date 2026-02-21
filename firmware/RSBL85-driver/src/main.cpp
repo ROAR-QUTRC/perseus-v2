9d2a02e1cc91e89a0fe70b1ccb53e9b7271ff3f8
OMEN44 <huonswales@gmail.com>
2026-02-21 20:44:23 +1000

    Feat: keyboard control

--- firmware/RSBL85-driver/src/main.cpp
+++ firmware/RSBL85-driver/src/main.cpp
@@ -16,9 +16,9 @@ constexpr int PAN = 2;
 constexpr int ELBOW = 1;
 
 std::vector<int> past_positions(3, 0);
+std::vector<double> target_positions = { 25.0 * 0.0, 25.0 * 0.0, 25 * 0.0 };
 
-void setup()
-{
+void setup() {
     Serial.begin(115200);
     Serial.println("RSBL85 CAN Bridge - Initializing");
 
@@ -32,28 +32,31 @@ void setup()
     past_positions[TILT] = servo.ReadPos(TILT);
     past_positions[PAN] = servo.ReadPos(PAN);
     past_positions[ELBOW] = servo.ReadPos(ELBOW);
+
+    double tilt_target = 0.0;
+    double pan_target = 0.0;
+    double elbow_target = 0.0;
+
+    target_positions[TILT] = 25.0 * tilt_target;
+    target_positions[PAN] = 25.0 * pan_target;
+    target_positions[ELBOW] = 25.0 * elbow_target;
 }
 
 std::vector<double> positions(3, 0);
 std::vector<int16_t> full_rev_count(3, 0);
-std::vector<double> target_positions = {50.0, 25.0 * -100.0, 25 * 0.0};
-void write_angle(int id, double angle)
-{
+void write_angle(int id, double angle) {
     target_positions[id] = angle;
 }
 
-const int ids[] = {TILT, PAN, ELBOW};
+const int ids [] = { TILT, PAN, ELBOW };
 
-void loop()
-{
-    for (unsigned i = 0; i < 3; i++)
-    {
+void loop() {
+    for (unsigned i = 0; i < 3; i++) {
         const int id = ids[i];
         const int read_pos = servo.ReadPos(id);
         const int diff = read_pos - past_positions[id];
 
-        if (abs(diff) > 2000)
-        {  // If the jump is greater than half a revolution then the encoder has wrapped around
+        if (abs(diff) > 2000) {  // If the jump is greater than half a revolution then the encoder has wrapped around
             bool clockwise = diff < 0;
             full_rev_count[id] += clockwise ? 1 : -1;
         }
@@ -68,19 +71,31 @@ void loop()
         constexpr double MAX_SPEED = 30000.0;
         const double error = target_positions[id] - positions[id];
 
-        if (abs(error) <= 1)
-        {
+        if (abs(error) <= 1) {
             servo.WriteSpe(id, 0, 0);
-        }
-        else if (abs(error) < 360)
-        {  // Reduce speed when 1 revolution from target
+        } else if (abs(error) < 360) {  // Reduce speed when 1 revolution from target
             servo.WriteSpe(id, error / 360.0 * MAX_SPEED, 0);
-        }
-        else
+        } else
             servo.WriteSpe(id, error > 0 ? MAX_SPEED : -MAX_SPEED, 0);
 
-        // if (id == ELBOW) {
-        Serial.printf("raw: (%s) %06d, rev: %03d\n", negative ? "-" : "+", raw_angle, full_rev_count[id]);
-        // }
+        const int key = Serial.read();
+        if (key != -1) {
+            switch (key) {
+                case 'w':
+                    target_positions[ELBOW] += 5.0;
+                    break;
+                case 's':
+                    target_positions[ELBOW] -= 5.0;
+                    break;
+                case 'a':
+                    target_positions[TILT] -= 5.0;
+                    break;
+                case 'd':
+                    target_positions[TILT] += 5.0;
+                    break;
+            }
+        }
     }
+
+    Serial.printf("Motor loads -> Tilt: %d, Pan: %d, Elbow: %d\n", servo.ReadLoad(TILT), servo.ReadLoad(PAN), servo.ReadLoad(ELBOW));
 }
