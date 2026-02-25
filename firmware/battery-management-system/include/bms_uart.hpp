dec994b9c49bb52f70ecebfa1385ab12c4960f9a
BrockMorgan88 <brockjamesmorgan@gmail.com>
2026-02-25 15:34:17 +0000

    feat: Implemented transmitting to BMS - Converting individual sections of message into array to send - Transmission generators for getting info and voltage from BMS

--- firmware/battery-management-system/include/bms_uart.hpp
+++ firmware/battery-management-system/include/bms_uart.hpp
@@ -1,18 +1,65 @@
 #include <chrono>
+#include <numeric>
+#include <vector>
 
 #include "driver/uart.h"
+
+class BmsUartMessage
+{
+public:
+    enum class status_byte_t : uint8_t
+    {
+        READ = 0xA5,
+        WRITE = 0x5A,
+    };
+    enum class command_byte_t : uint8_t
+    {
+        INFORMATION = 0x03,
+        VOLTAGE = 0x04,
+        VERSION = 0x05,
+        MOSFET_CONTROL = 0xE1,
+    };
+    BmsUartMessage(command_byte_t command, std::vector<uint8_t>& data);
+    uint8_t get_start_byte() { return _start_byte; }
+    status_byte_t get_status_byte() { return _status_byte; }
+    command_byte_t get_command_byte() { return _command_byte; }
+    uint8_t get_data_length() { return _data_length; }
+    std::vector<uint8_t> get_data() { return _data; };
+    uint16_t get_checksum() { return _checksum; }
+    uint8_t get_stop_byte() { return _stop_byte; }
+
+private:
+    uint16_t _convert_to_twos_complement(uint16_t number);
+    uint8_t _start_byte = 0xDD;
+    status_byte_t _status_byte;
+    command_byte_t _command_byte;
+    uint8_t _data_length = 0;
+    std::vector<uint8_t> _data;
+    uint16_t _checksum;
+    uint8_t _stop_byte = 0x77;
+};
 
 class BmsUartDriver
 {
 public:
     BmsUartDriver(uart_port_t port_number);
     ~BmsUartDriver();
+    const std::chrono::steady_clock::duration get_voltage_interval()
+    {
+        return _voltage_interval;
+    }
+    const std::chrono::steady_clock::duration get_information_interval()
+    {
+        return _information_interval;
+    }
+    bool transmit(BmsUartMessage& message);
+    bool receive(BmsUartMessage& message);
 
 private:
-    int convert_to_twos_complement(int number);
     const int BUFFER_SIZE = 1024;
     uart_port_t _uart_port_number;
     uart_config_t _uart_config = {
+        // BMS is 9600 baud 8N1
         .baud_rate = 9600,
         .data_bits = UART_DATA_8_BITS,
         .parity = UART_PARITY_DISABLE,
--- firmware/battery-management-system/src/bms_uart.cpp
+++ firmware/battery-management-system/src/bms_uart.cpp
@@ -10,3 +10,57 @@ BmsUartDriver::~BmsUartDriver()
 {
     ESP_ERROR_CHECK(uart_driver_delete(_uart_port_number));
 }
+bool BmsUartDriver::transmit(BmsUartMessage& message)
+{
+    try
+    {
+        std::vector<uint8_t> data{message.get_start_byte(), static_cast<uint8_t>(message.get_status_byte()), static_cast<uint8_t>(message.get_command_byte()), message.get_data_length()};
+        if (message.get_data_length())
+        {
+            data.insert(data.end(), message.get_data().begin(), message.get_data().end());
+        }
+        data.emplace_back((message.get_checksum() && 0xFF00) >> 8);
+        data.emplace_back(message.get_checksum() && 0xFF);
+        data.emplace_back(message.get_stop_byte());
+        if (uart_write_bytes(_uart_port_number, data.data(), data.size()) == -1)
+        {
+            printf("ESP error when transmitting data request!\n");
+            return 1;
+        }
+    }
+    catch (std::exception& e)
+    {
+        printf("Error when formatting data to transmit: '%s'\n", e.what());
+        return 1;
+    }
+    return 0;
+}
+bool BmsUartDriver::receive(BmsUartMessage& message)
+{
+    // TODO: Implement receiving messages
+}
+
+BmsUartMessage::BmsUartMessage(command_byte_t command, std::vector<uint8_t>& data) : _command_byte(command), _data(data)
+{
+    if (command == command_byte_t::MOSFET_CONTROL)
+    {
+        _status_byte = status_byte_t::WRITE;
+    }
+    else
+    {
+        _status_byte = status_byte_t::READ;
+    }
+    uint16_t sum = static_cast<uint8_t>(command);
+    if (!data.empty())
+    {
+        _data_length = data.size();
+        sum = std::accumulate(data.begin(), data.end(), static_cast<uint8_t>(command) + _data_length);
+    }
+
+    _checksum = _convert_to_twos_complement(sum);
+}
+uint16_t BmsUartMessage::_convert_to_twos_complement(uint16_t number)
+{
+    uint16_t inverse = ~number;
+    return (inverse + 1);
+}
