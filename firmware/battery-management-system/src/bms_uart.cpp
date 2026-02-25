59e902b37867fad9eb6712ba131d4030d04a4fb2
BrockMorgan88 <166275321+BrockMorgan88@users.noreply.github.com>
2026-02-25 10:25:01 +0000

    chore: Format and lint

--- firmware/battery-management-system/src/bms_uart.cpp
+++ firmware/battery-management-system/src/bms_uart.cpp
@@ -1,6 +1,7 @@
 #include "bms_uart.hpp"
 
-BmsUartDriver::BmsUartDriver(uart_port_t uart_port_number) : _uart_port_number(uart_port_number)
+BmsUartDriver::BmsUartDriver(uart_port_t uart_port_number)
+    : _uart_port_number(uart_port_number)
 {
     ESP_ERROR_CHECK(uart_driver_install(uart_port_number, BUFFER_SIZE, BUFFER_SIZE, 0, NULL, 0));
     ESP_ERROR_CHECK(uart_param_config(uart_port_number, &_uart_config));
