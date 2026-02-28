3aa25dcba0f72f3c6f733f93fba5da6cd89252a1
bocho0600 <134352794+bocho0600@users.noreply.github.com>
2026-02-28 22:27:03 +0000

    chore: Format and lint

--- software/ros_ws/src/autonomy/launch/domain_bridge.launch.py
+++ software/ros_ws/src/autonomy/launch/domain_bridge.launch.py
@@ -20,13 +20,11 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-import os
 
 
 def generate_launch_description():
     """Generate launch description for domain bridge."""
-    
+
     # Declare Arguments
     source_domain_arg = DeclareLaunchArgument(
         "source_domain",
