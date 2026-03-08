86cd6790809d78966cc3e0af618e62fd32a671bf
LucaLubrano <lucalub123@gmail.com>
2026-03-08 15:20:39 +1000

    fast-lio working in gazebo sim

--- software/ros_ws/src/perseus_mapping/launch/perseus_mapping.launch.py
+++ software/ros_ws/src/perseus_mapping/launch/perseus_mapping.launch.py
@@ -1,41 +1,55 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.substitutions import PathJoinSubstitution
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
 import os
-
+import yaml
 
 def generate_launch_description():
-    # Config file for the livox mid360 lidar on perseus
-    # Note: this is a clone of the mid360.yaml on github/fast-lio with tested parameters
-    config_file = os.path.join(
+    config_path = os.path.join(
         get_package_share_directory("perseus_mapping"),
         "config",
-        "livox_mid360.yaml",
     )
 
-    # Launches the default fast-lio mapping launch file
+    launch_config_file = os.path.join(config_path, "launch_config.yaml")
+    with open(launch_config_file, 'r') as f:
+        launch_config = yaml.safe_load(f)
+
+    use_sim_time = str(launch_config.get("use_sim_time", "false")).lower()
+    rviz = str(launch_config.get("rviz", "false")).lower()
+    sim = str(launch_config.get("sim", "false")).lower()
+
     fast_lio_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            [
-                PathJoinSubstitution(
-                    [
-                        FindPackageShare("fast_lio"),
-                        "launch",
-                        "mapping.launch.py",
-                    ]
-                )
-            ]
+            PathJoinSubstitution([
+                FindPackageShare("fast_lio"),
+                "launch",
+                "mapping.launch.py",
+            ])
         ),
         launch_arguments={
-            "config_file": config_file,
+            "config_path": config_path,
+            "config_file": "livox_mid360.yaml",
+            "rviz": rviz,
+            "use_sim_time": use_sim_time,
         }.items(),
     )
 
-    return LaunchDescription(
-        [
-            fast_lio_launch,
-        ]
+    sim_filter_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                FindPackageShare("perseus_mapping"),
+                "launch",
+                "sim_pointcloud_filter.launch.py",
+            ])
+        ),
+        condition=IfCondition(sim),
     )
+
+    return LaunchDescription([
+        sim_filter_launch,
+        fast_lio_launch,
+    ])
