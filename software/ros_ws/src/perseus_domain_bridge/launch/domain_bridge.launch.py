9dcf164138662d15ed76b7f7846887bcb61fec26
UltraFishy <103304914+UltraFishy@users.noreply.github.com>
2026-03-15 06:04:35 +0000

    chore: Format and lint

--- software/ros_ws/src/perseus_domain_bridge/launch/domain_bridge.launch.py
+++ software/ros_ws/src/perseus_domain_bridge/launch/domain_bridge.launch.py
@@ -10,37 +10,43 @@ from launch_ros.actions import Node
 
 
 def launch_setup(context, *args, **kwargs):
-    is_rover = LaunchConfiguration('rover').perform(context).lower() in ('true', '1', 'yes')
-    domain_id = int(LaunchConfiguration('domain_id').perform(context))
+    is_rover = LaunchConfiguration("rover").perform(context).lower() in (
+        "true",
+        "1",
+        "yes",
+    )
+    domain_id = int(LaunchConfiguration("domain_id").perform(context))
 
     config_filename = (
         "autonomous_to_main.yaml" if is_rover else "main_to_autonomous.yaml"
     )
 
     config_file = os.path.join(
-        get_package_share_directory('your_package_name'),  # <-- replace with your package name
-        'config',
-        config_filename
+        get_package_share_directory(
+            "your_package_name"
+        ),  # <-- replace with your package name
+        "config",
+        config_filename,
     )
 
     # Load the config and override from_domain with the launch arg
-    with open(config_file, 'r') as f:
+    with open(config_file, "r") as f:
         config = yaml.safe_load(f)
 
-    config['from_domain'] = domain_id
+    config["from_domain"] = domain_id
 
     # Write to a temp file for domain_bridge to consume
     tmp = tempfile.NamedTemporaryFile(
-        mode='w', suffix='.yaml', delete=False, prefix='domain_bridge_'
+        mode="w", suffix=".yaml", delete=False, prefix="domain_bridge_"
     )
     yaml.dump(config, tmp)
     tmp.close()
 
     domain_bridge_node = Node(
-        package='domain_bridge',
-        executable='domain_bridge',
-        name='domain_bridge',
-        output='screen',
+        package="domain_bridge",
+        executable="domain_bridge",
+        name="domain_bridge",
+        output="screen",
         arguments=[tmp.name],
     )
 
@@ -49,19 +55,21 @@ def launch_setup(context, *args, **kwargs):
 
 def generate_launch_description():
     rover_arg = DeclareLaunchArgument(
-        'rover',
-        default_value='false',
-        description='Set to true when launching on the rover, false for the main station'
+        "rover",
+        default_value="false",
+        description="Set to true when launching on the rover, false for the main station",
     )
 
     domain_id_arg = DeclareLaunchArgument(
-        'domain_id',
-        default_value='0',  # rover default; pass 2 on main station
-        description='ROS_DOMAIN_ID of this device (from_domain). Rover=0, Main=2'
+        "domain_id",
+        default_value="0",  # rover default; pass 2 on main station
+        description="ROS_DOMAIN_ID of this device (from_domain). Rover=0, Main=2",
     )
 
-    return LaunchDescription([
-        rover_arg,
-        domain_id_arg,
-        OpaqueFunction(function=launch_setup),
-    ])
+    return LaunchDescription(
+        [
+            rover_arg,
+            domain_id_arg,
+            OpaqueFunction(function=launch_setup),
+        ]
+    )
