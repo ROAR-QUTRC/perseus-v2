687c89b501e0c6c466de931b53d941b8d16859dc
BrockMorgan88 <brockjamesmorgan@gmail.com>
2026-03-15 20:14:57 +1000

    fix: Domain bridge recognises config file argument

--- software/ros_ws/src/perseus_domain_bridge/launch/domain_bridge.launch.py
+++ software/ros_ws/src/perseus_domain_bridge/launch/domain_bridge.launch.py
@@ -1,31 +1,13 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import (
-    LaunchConfiguration,
-    IfElseSubstitution,
-    AndSubstitution,
-    NotEqualsSubstitution,
-    PathJoinSubstitution,
-)
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
-from launch.launch_description_sources import FrontendLaunchDescriptionSource
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    domain_id_autonomy = LaunchConfiguration("domain_id_autonomy")
-    domain_id_main = LaunchConfiguration("domain_id_main")
     config_filename = LaunchConfiguration("config_filename")
     arguments = [
-        DeclareLaunchArgument(
-            "domain_id_main",
-            default_value="",
-            description="ROS_DOMAIN_ID of this device (to_domain). Rover=0, Main=2",
-        ),
-        DeclareLaunchArgument(
-            "domain_id_autonomy",
-            default_value="",
-            description="ROS_DOMAIN_ID of this device (from_domain). Rover=0, Main=2",
-        ),
         DeclareLaunchArgument(
             "config_filename",
             default_value="big-brain-bridge.yaml",
@@ -39,31 +21,15 @@ def generate_launch_description():
             config_filename,
         ],
     )
-    remap_ids_dict = {
-        "--from": domain_id_autonomy,
-        "--to": domain_id_main,
-        "config": config_file,
-    }
-    domain_bridge_launch = IncludeLaunchDescription(
-        FrontendLaunchDescriptionSource(
-            [
-                PathJoinSubstitution(
-                    [
-                        FindPackageShare("domain_bridge"),
-                        "launch",
-                        "domain_bridge.launch.xml",
-                    ],
-                ),
-            ]
-        ),
-        launch_arguments=IfElseSubstitution(
-            AndSubstitution(
-                NotEqualsSubstitution(domain_id_autonomy, ""),
-                NotEqualsSubstitution(domain_id_main, ""),
-            ),
-            remap_ids_dict.items(),
-            {"config": config_file}.items(),
-        ),
+    domain_bridge_node = Node(
+        executable="domain_bridge",
+        package="domain_bridge",
+        ros_arguments=["--log-level", "DEBUG"],
+        arguments=[
+            config_file,
+        ],
     )
-    launch_files = [domain_bridge_launch]
-    return LaunchDescription([arguments, launch_files])
+    nodes = [
+        domain_bridge_node,
+    ]
+    return LaunchDescription(arguments + nodes)
