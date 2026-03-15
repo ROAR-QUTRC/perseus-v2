5996a2908ef1523c095b7fd05b623d39c2901109
BrockMorgan88 <brockjamesmorgan@gmail.com>
2026-03-15 17:39:28 +1000

    Feat: Refactor to have one domain_bridge config file, fixed launch file to add remapping of domains

--- software/ros_ws/src/perseus_domain_bridge/launch/domain_bridge.launch.py
+++ software/ros_ws/src/perseus_domain_bridge/launch/domain_bridge.launch.py
@@ -1,75 +1,69 @@
-import os
-import tempfile
-
-import yaml
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, OpaqueFunction
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
-
-
-def launch_setup(context, *args, **kwargs):
-    is_rover = LaunchConfiguration("rover").perform(context).lower() in (
-        "true",
-        "1",
-        "yes",
-    )
-    domain_id = int(LaunchConfiguration("domain_id").perform(context))
-
-    config_filename = (
-        "autonomous_to_main.yaml" if is_rover else "main_to_autonomous.yaml"
-    )
-
-    config_file = os.path.join(
-        get_package_share_directory(
-            "your_package_name"
-        ),  # <-- replace with your package name
-        "config",
-        config_filename,
-    )
-
-    # Load the config and override from_domain with the launch arg
-    with open(config_file, "r") as f:
-        config = yaml.safe_load(f)
-
-    config["from_domain"] = domain_id
-
-    # Write to a temp file for domain_bridge to consume
-    tmp = tempfile.NamedTemporaryFile(
-        mode="w", suffix=".yaml", delete=False, prefix="domain_bridge_"
-    )
-    yaml.dump(config, tmp)
-    tmp.close()
-
-    domain_bridge_node = Node(
-        package="domain_bridge",
-        executable="domain_bridge",
-        name="domain_bridge",
-        output="screen",
-        arguments=[tmp.name],
-    )
-
-    return [domain_bridge_node]
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import (
+    LaunchConfiguration,
+    IfElseSubstitution,
+    AndSubstitution,
+    NotEqualsSubstitution,
+    PathJoinSubstitution,
+)
+from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import FrontendLaunchDescriptionSource
 
 
 def generate_launch_description():
-    rover_arg = DeclareLaunchArgument(
-        "rover",
-        default_value="false",
-        description="Set to true when launching on the rover, false for the main station",
-    )
-
-    domain_id_arg = DeclareLaunchArgument(
-        "domain_id",
-        default_value="0",  # rover default; pass 2 on main station
-        description="ROS_DOMAIN_ID of this device (from_domain). Rover=0, Main=2",
-    )
-
-    return LaunchDescription(
+    domain_id_autonomy = LaunchConfiguration("domain_id_autonomy")
+    domain_id_main = LaunchConfiguration("domain_id_main")
+    config_filename = LaunchConfiguration("config_filename")
+    arguments = [
+        DeclareLaunchArgument(
+            "domain_id_main",
+            default_value="",
+            description="ROS_DOMAIN_ID of this device (to_domain). Rover=0, Main=2",
+        ),
+        DeclareLaunchArgument(
+            "domain_id_autonomy",
+            default_value="",
+            description="ROS_DOMAIN_ID of this device (from_domain). Rover=0, Main=2",
+        ),
+        DeclareLaunchArgument(
+            "config_filename",
+            default_value="big-brain-bridge.yaml",
+            description="Config file for the domain bridge - inside perseus_domain_bridge/config",
+        ),
+    ]
+    config_file = PathJoinSubstitution(
         [
-            rover_arg,
-            domain_id_arg,
-            OpaqueFunction(function=launch_setup),
-        ]
+            FindPackageShare("perseus_domain_bridge"),
+            "config",
+            config_filename,
+        ],
     )
+    remap_ids_dict = {
+        "--from": domain_id_autonomy,
+        "--to": domain_id_main,
+        "config": config_file,
+    }
+    domain_bridge_launch = IncludeLaunchDescription(
+        FrontendLaunchDescriptionSource(
+            [
+                PathJoinSubstitution(
+                    [
+                        FindPackageShare("domain_bridge"),
+                        "launch",
+                        "domain_bridge.launch.xml",
+                    ],
+                ),
+            ]
+        ),
+        launch_arguments=IfElseSubstitution(
+            AndSubstitution(
+                NotEqualsSubstitution(domain_id_autonomy, ""),
+                NotEqualsSubstitution(domain_id_main, ""),
+            ),
+            remap_ids_dict.items(),
+            {"config": config_file}.items(),
+        ),
+    )
+    launch_files = [domain_bridge_launch]
+    return LaunchDescription([arguments, launch_files])
