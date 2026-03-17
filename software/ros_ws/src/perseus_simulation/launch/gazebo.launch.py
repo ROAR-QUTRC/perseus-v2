73439acadac6d0c86e6e4daea3b4b89f0a605cc1
bocho0600 <nguyenleminh1002@gmail.com>
2026-03-17 22:56:05 +1000

    restore simulation launch file

--- software/ros_ws/src/perseus_simulation/launch/gazebo.launch.py
+++ software/ros_ws/src/perseus_simulation/launch/gazebo.launch.py
@@ -42,7 +42,7 @@ def generate_launch_description():
         ),
         DeclareLaunchArgument(
             "initial_pose_y",
-            default_value="3.0",
+            default_value="-3.0",
             description="Initial Y position of the robot",
         ),
         DeclareLaunchArgument(
@@ -67,7 +67,11 @@ def generate_launch_description():
         )
         gz_launch = ExecuteProcess(
             cmd=[
-                "nixgl",
+                "nix",
+                "run",
+                "--impure",
+                "github:nix-community/nixGL",
+                "--",
                 "ros2",
                 "launch",
                 "ros_gz_sim",
