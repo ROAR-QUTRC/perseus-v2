cdf33ef7f71f024eacb14bf7636018a3d855439b
UltraFishy <103304914+UltraFishy@users.noreply.github.com>
2026-03-20 06:40:16 +0000

    chore: Format and lint

--- software/shared/hi-can/include/hi_can_address.hpp
+++ software/shared/hi-can/include/hi_can_address.hpp
@@ -454,10 +454,10 @@ namespace hi_can
             namespace control
             {
                 constexpr uint8_t SUBSYSTEM_ID = 0x00;
-                namespace colour 
+                namespace colour
                 {
                     constexpr uint8_t DEVICE = 0x00;
-                    enum class group 
+                    enum class group
                     {
                         RING = 0x00,
                     };
@@ -468,7 +468,7 @@ namespace hi_can
                     };
                 }
             }
-        } 
+        }
 
         // legacy addresses for old hardware
         /// @brief Namespace containing all addresses in the legacy system
