016b1ed2e8f5c0f5f28c4b0443e518bd2bf12e98
test-account30 <matthewallen.yt@gmail.com>
2026-03-21 19:00:53 +1000

    removing redundant config settings

--- software/shared/hi-can-generator/setup.py
+++ software/shared/hi-can-generator/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="hi-can-generator",
     version="0.0.1",
-    packages=find_packages(),
+    packages=[],
     scripts=["hi-can-generator.py"],
 )
