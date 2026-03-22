396f9d56a1e13f8ed205f44b33e5d1fcbeca7c60
bocho0600 <nguyenleminh1002@gmail.com>
2026-03-23 01:29:37 +1000

    feat: add support for regions of interest in detection results

--- software/ros_ws/src/perseus_vision/include/perseus_vision/cube_detector.hpp
+++ software/ros_ws/src/perseus_vision/include/perseus_vision/cube_detector.hpp
@@ -186,6 +186,7 @@ namespace perseus_vision
         std::string _latest_detections_frame_id;
         std::vector<int32_t> _latest_detection_ids;
         std::vector<geometry_msgs::msg::Pose> _latest_detection_poses;
+        std::vector<sensor_msgs::msg::RegionOfInterest> _latest_detection_regions_of_interest;
         std::vector<cv::Rect> _latest_detection_bboxes;
         std::string _latest_detection_message{"No cube detections are currently cached."};
         cv::Mat _latest_annotated_frame;
