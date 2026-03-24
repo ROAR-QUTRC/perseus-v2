a05f4d3eecce487003e26c9f4088ccec4e02764e
Tracheu <ttycheung13@gmail.com>
2026-03-24 19:36:10 +1000

    Feat: Updated nix package - Brock

--- software/machine_learning/default.nix
+++ software/machine_learning/default.nix
@@ -6,6 +6,8 @@ python3Packages.buildPythonPackage {
   pname = "roar_ml";
   version = "0.0.1";
   src = ./.;
+  pyproject = true;
+  build-system = [  python3Packages.setuptools];
 
   propagatedBuildInputs = [
     python3Packages.torch
