9b413f7be1564b155d5feb65f494555790e692ab
ZephCreations <65759870+ZephCreations@users.noreply.github.com>
2026-03-25 16:32:04 +1030

    feat: Added CAN-debug UI

---
+++ software/web_ui/src/lib/widgets/canDebug.svelte
@@ -0,0 +1,131 @@
+<script lang="ts" module>
+	// This is to expose the widget settings to the panel. Code in here will only run once when the widget is first loaded.
+	import type { WidgetGroupType, WidgetSettingsType } from '$lib/scripts/state.svelte';
+
+	export const name = 'Can Debug';
+	export const description = 'Convert console CAN messages to human-readable part name';
+	export const group: WidgetGroupType = 'CAN Bus';
+
+	export const settings: WidgetSettingsType = $state<WidgetSettingsType>({
+		groups: {}
+	});
+	
+</script>
+
+<script lang="ts">
+	import { io, type Socket } from 'socket.io-client';
+	import { onMount } from 'svelte';
+	import { ScrollArea } from '$lib/components/ui/scroll-area/index';
+
+	let canMsgs = $state<
+		{
+			address: string;
+			system: string;
+			subsystem: string;
+			device: string;
+			group: string;
+			parameter: string | null;
+			latestData: number;
+		}[]
+	>([]);
+
+	// Each value object
+	interface CanLutEntry {
+		system: string;
+		subsystem: string;
+		device: string;
+		group: string;
+		parameter: string | null;
+	}
+	
+	// full lookup table
+	interface CanData {
+		iface: string;
+		address: string;
+		details: CanLutEntry;
+		data: number;
+	}
+
+	const socket: Socket = io();
+
+	// On update from socket
+	socket.on('can-data', (dataList: Array<CanData>) => {
+		console.log("RECEIVING...");
+		console.log("DATA", dataList);
+
+		dataList.forEach((value, index) => {
+			console.log(value);
+			const inArray = canMsgs.findIndex((msg) => msg.address === value.address);
+
+			if (inArray === -1) {
+				// Add new row
+				canMsgs.push({
+					address: value.address,
+					system: value.details.system,
+					subsystem: value.details.subsystem,
+					device: value.details.device,
+					group: value.details.group,
+					parameter: value.details.parameter,
+					latestData: value.data
+				});
+				return;
+			}
+
+			// Update data
+			canMsgs[index].latestData = value.data;
+			// console.log(JSON.stringify(data, null, 2));
+		})
+	});
+
+	// On creation of widget
+	onMount(() => {
+		// Create charts/interfaces
+		console.log("Mounted");
+
+		return () => {
+			socket.disconnect();
+		};
+	});
+
+</script>
+
+<ScrollArea class="h-full -m-2" orientation="vertical">
+	<div class="relative h-full w-full">
+		<table class="w-full table-auto">
+			<thead>
+				<tr class="border-b">
+					<th class="border-r p-2">System</th>
+					<th class="p-2">Sub System</th>
+					<th class="p-2">Device</th>
+					<th class="p-2">Group</th>
+					<th class="p-2">Parameter</th>
+					<th class="border-l p-2">Latest Data</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each canMsgs as canMsg}
+					<tr class="border border-l-0 border-x-0">
+						<td class="p-2 border-r">
+							<p class="w-full text-center mb-2"><b>{canMsg.system}</b></p>
+						</td>
+						<td class="p-2">
+							<p class="w-full text-center mb-2">{canMsg.subsystem}</p>
+						</td>
+						<td class="p-2">
+							<p class="w-full text-center mb-2">{canMsg.device}</p>
+						</td>
+						<td class="p-2">
+							<p class="w-full text-center mb-2">{canMsg.group}</p>
+						</td>
+						<td class="p-2">
+							<p class="w-full text-center mb-2">{canMsg.parameter}</p>
+						</td>
+						<td class=" w-full overflow-hidden border-l p-2">
+							<p class="w-full text-center mb-2">{canMsg.latestData}</p>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
+</ScrollArea>
---
+++ software/web_ui/src/server/scripts/canSocket.js
@@ -0,0 +1,149 @@
+// @ts-nocheck
+import { spawn } from "child_process";
+import path from 'path';
+import fs from 'node:fs';
+import { response } from "express";
+
+const hiCanGeneratorFileLocation = path.resolve("../shared/hi-can-generator/hi-can-generator.py");
+const hiCanAddressFileLocation = path.resolve("../shared/hi-can/include/hi_can_address.hpp");
+const canLookupFilePath = path.resolve('src/lib/canLookup.json');
+
+let timeoutId;
+let running = false;
+
+let canLookup = null;
+const buffer = [];
+const MAX_BUFFER = 1000;
+
+export const canSocket = (socket) => {
+	if (!running) {
+		console.log("Can Socket");
+		// Create lookup table file on launch
+		generateFile();
+
+		// Start processing candump output
+		const stopCandump = startCanDump("vcan0");
+
+		// Send messages as they are recieved
+		timeoutId = setInterval(() => {
+			if (buffer.length === 0) return;
+			const batch = buffer.splice(0, buffer.length);
+			socket.emit("can-data", batch);
+		}, 2000);
+
+		running = true;
+	}
+};
+
+async function generateFile() {
+	try {
+		const data = await lookupTable();
+		
+		canLookup = data;
+		fs.writeFileSync(canLookupFilePath, JSON.stringify(data, null, 2));
+
+		console.log("File Written");
+	} catch (err) {
+		console.error("Error:", err);
+	}
+}
+
+const lookupTable = () => {
+	console.log("LOOKUP TABLE");
+	return new Promise((resolve, reject) => {
+		const pythonProcess = spawn("python3", [hiCanGeneratorFileLocation, hiCanAddressFileLocation]);
+
+		let output = "";
+		let error = "";
+
+		pythonProcess.stdout.on("data", (data) => {
+			output += data.toString();
+		});
+
+		pythonProcess.stderr.on("data", (data) => {
+			error += data.toString();
+		});
+
+		pythonProcess.on("close", (code) => {
+			if (code !== 0) {
+				reject(error);
+				return;
+			}
+
+			// console.log(output.slice(output.length-200,output.length), "\n");
+
+			try {
+				canLookup = JSON.parse(output);
+				console.log("CAN LOOKUP TYPE:", typeof canLookup)
+				resolve(JSON.parse(output));
+			} catch (err) {
+				console.log(typeof output);
+				console.error(err);
+				reject(`Invalid JSON\n ${output.slice(0,200)}\n...\n${output.slice(output.length-200,output.length)}`);
+			}
+		});
+	});
+};
+
+const getCanData = () => {
+	return new Promise((resolve, reject) => {
+		const temp = latestInfo;
+		latestInfo = [];
+		resolve(latestInfo);
+	});
+};
+
+export function startCanDump(iface) {
+	const proc = spawn("candump", [iface]);
+
+	let leftover = "";
+
+	proc.stdout.on("data", (data) => {
+		leftover += data.toString();
+
+		const lines = leftover.split("\n");
+		leftover = lines.pop() || "";
+
+		for (const line of lines) {
+			if (!line.trim()) continue;
+
+			const parsed = parseCandump(line);
+			if (parsed) {
+				buffer.push(parsed);
+				console.log("Added: ", parsed);
+
+				// avoid memory overflow
+				if (buffer.length > MAX_BUFFER) {
+					buffer.shift();
+				}
+			}
+		}
+	});
+
+	proc.stderr.on("data", (d) => {
+		console.error("STDERR:", d.toString());
+	});
+
+	return () => proc.kill() // stop function
+}
+
+function parseCandump(line) {
+	const parts = line.trim().split(/\s+/);
+	if (parts.length < 4) {
+		return null;
+	}
+	const address = `0x${parseInt(parts[1], 16).toString(16).padStart(8, "0")}`;
+	const canDetails = canLookup[address];
+
+	const parsed = {
+		iface: parts[0],
+		address: address,
+		details: canDetails,
+		data: parts.slice(3)
+	};
+
+	// console.log("Parsing input", parsed);
+
+	return parsed 
+}
+
