4fc1dd4622f1fdaf5e4d5d8d16e5d474fe96986e
bocho0600 <nguyenleminh1002@gmail.com>
2026-06-14 13:42:15 +1000

    chore: Add vikit-common and vikit-ros packages to overlay.nix

--- software/ros_ws/third-party-packages/overlay.nix
+++ software/ros_ws/third-party-packages/overlay.nix
@@ -4,6 +4,8 @@ let
     livox-ros-driver2 = individualFinal.callPackage ./livox-ros-driver2 { };
     fast-lio = individualFinal.callPackage ./fast-lio { };
     ndt-omp-ros2 = individualFinal.callPackage ./ndt-omp-ros2 { };
+    vikit-common = individualFinal.callPackage ./vikit-common { };
+    vikit-ros = individualFinal.callPackage ./vikit-ros { };
   };
 in
 prev.lib.composeManyExtensions [
